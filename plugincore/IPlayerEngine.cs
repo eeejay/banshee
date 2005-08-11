ff0762b33aee56848b9280df864662fae9583e7f
Aaron Bockover <abock@src.gnome.org>
2005-08-11 19:31:55 +0000

    IPlayerEngine interface updates

--- plugincore/IPlayerEngine.cs
+++ plugincore/IPlayerEngine.cs
@@ -25,11 +25,69 @@
  *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
  *  DEALINGS IN THE SOFTWARE.
  */
+ 
+/* 
+	Banshee supports runtime-loadable player engines, such as a GStreamer
+	or Helix player. A Player Engine is a class that implements the 
+	Banshee.IPlayerEngine interface. The assembly containing this class
+	and any necesary files (such as a shared library that the engine class
+	P/Invokes into) must be installed in :
+	
+		/path/to/banshee/install/mediaengines/<config-name>.
+
+	Banshee will detect the assembly with a class implementing the
+	Banshee.IPlayerEngine interface.
+	
+	After detection, Banshee creates an instance of the implementing
+	class. Because of this, the class SHOULD NOT perform any engine
+	initialization routines in the constructor! Banshee needs an instance
+	to read required fields which must contain information about the engine.
+	
+	The Initialize() method is used to perform all engine initialization,
+	and the Finalize() method is used to perform any necessary engine
+	cleanup. DO NOT USE the constructor/destructor for this purpose!
+*/
 
 using System;
 
 namespace Banshee
-{
+{	
+	public interface IPlayerEngine
+	{
+		// Signals which must be implemented in order for a player engine
+		// to behave properly with the Banshee UI.
+		event  PlayerEngineErrorHandler Error;
+		event  PlayerEngineVolumeChangedHandler VolumeChanged;
+		event  PlayerEngineIterateHandler Iterate;
+		event  EventHandler EndOfStream;
+	
+		void   Initialize();           // actual engine initialization
+		void   TestInitialize();       // test Initialize and Finalize
+		void   Dispose();              // engine finalization/shutdown
+		
+		bool   Open(ITrackInfo track); // load a file into the engine
+		void   Close();                // close an open file
+		
+		void   Play();                 // play a file
+		void   Pause();                // pause a playing file
+
+		bool   Loaded         { get; } // return true if file is loaded
+		bool   Playing        { get; } // return true if file is playing
+		
+		ushort Volume         { get; set; } // volume 0-100
+		uint   Position       { get; set; } // position 0-N (seconds)
+		uint   Length         { get; } // Length of stream, 0 if unavailable
+
+		string ConfigName     { get; } // GConf configuration name (gst/helix)
+		string EngineName     { get; } // Short Display Name (GStreamer/Helix)
+		string EngineLongName { get; } // Long Name (Helix hxclientkit Engine)
+		string EngineDetails  { get; } // Describe the engine/framework
+		string AuthorName     { get; } // Name of Engine Implementation Author
+		string AuthorEmail    { get; } // Email address of above
+		int    MajorVersion   { get; } // Major version of Engine
+		int    MinorVersion   { get; } // Minor version of Engine
+	}
+	
 	public delegate void PlayerEngineErrorHandler(object o, 
 		PlayerEngineErrorArgs args);
 	
@@ -46,43 +104,11 @@ namespace Banshee
 	
 	public class PlayerEngineVolumeChangedArgs : EventArgs
 	{
-		public double Volume;
+		public ushort Volume;
 	}
 	
 	public class PlayerEngineIterateArgs : EventArgs
 	{
-		public long Position;
-	}
-	
-	public interface IPlayerEngine
-	{
-		event  PlayerEngineErrorHandler Error;
-		event  PlayerEngineVolumeChangedHandler VolumeChanged;
-		event  PlayerEngineIterateHandler Iterate;
-		event  EventHandler EndOfStream;
-	
-		void   Initialize();
-		void   TestInitialize();
-		
-		bool   Open(ITrackInfo track);
-		
-		void   Play();
-		void   Pause();
-		void   Shutdown();
-
-		bool   Loaded         { get; }
-		bool   Playing        { get; }
-		
-		double Volume         { get; set; }
-		long   Position       { get; set; }
-
-		string ConfigName     { get; }
-		string EngineName     { get; }
-		string EngineLongName { get; }
-		string EngineDetails  { get; }
-		string AuthorName     { get; }
-		string AuthorEmail    { get; }
-		int    MajorVersion   { get; }
-		int    MinorVersion   { get; }
+		public uint Position;
 	}
 }
