dfa455b59832a084069da17c0b651592fc22ed49
Aaron Bockover <abock@src.gnome.org>
2005-08-18 20:51:07 +0000

    Scrolling credits, woo... thanks Evan?

--- src/AboutBox.cs
+++ src/AboutBox.cs
@@ -30,17 +30,18 @@ using System;
 using System.Reflection;	
 using Gtk;
 using Glade;
+using Gdk;
+using GLib;
+using Pango;
+using System.Text;
 
 namespace Banshee
 {
 	public class AboutBox
 	{
-		[Widget] private Window WindowAbout;
-		[Widget] private Image ImageSplash;
+		[Widget] private Gtk.Window WindowAbout;
 		[Widget] private TreeView VersionTreeView;
-		[Widget] private Label AboutLabel;
 		[Widget] private Label LabelQuickInfo;
-		[Widget] private Label LabelCredits;
 		
 		private TreeStore VersionStore;
 
@@ -49,12 +50,13 @@ namespace Banshee
 			Glade.XML glade = new Glade.XML(null, 
 				"about.glade", "WindowAbout", null);
 			glade.Autoconnect(this);
-			ImageSplash.Pixbuf = 
-				Gdk.Pixbuf.LoadFromResource("sonance-splash.png");
 			WindowAbout.Icon = Gdk.Pixbuf.LoadFromResource("sonance-icon.png");
+			(glade["CreditsContainer"] as Container).Add(new ScrollBox());
+			glade["CreditsContainer"].ShowAll();
+			(glade["AboutHeader"] as Gtk.Image).Pixbuf = 
+				Gdk.Pixbuf.LoadFromResource("about-header.png");
 			LoadAboutInfo();
 			BuildVersionInfo();
-			LoadCredits();
 		}
 		
 		private void OnButtonCloseClicked(object o, EventArgs args)
@@ -65,26 +67,10 @@ namespace Banshee
 		private void LoadAboutInfo()
 		{
 			AssemblyName selAsm = Assembly.GetEntryAssembly().GetName();
-
-			AboutLabel.Markup = String.Format(
-				"<span weight=\"bold\">Version</span>\n" + 
-				"   {0} v{1}.{2}.{3}\n\n" + 
-				"<span weight=\"bold\">License</span>\n" + 
-				"   Released under the MIT License\n\n" + 
-				"<span weight=\"bold\">Author</span>\n" + 
-				"   Written by Aaron Bockover\n" + 
-				"   <span color=\"blue\">http://sonance.aaronbock.net</span>\n" + 
-				"   <span color=\"blue\">aaron@aaronbock.net</span>\n\n"  +
-				"<span weight=\"bold\">Copyright</span>\n" +
-				"   Copyright (C) 2005 Novell\n" + 
-				"   Copyright (C) 2005 Aaron Bockover\n",
-				StringUtil.UcFirst(selAsm.Name), selAsm.Version.Major, 
-				selAsm.Version.Minor, selAsm.Version.Build
-			);
 			
 			LabelQuickInfo.Markup = String.Format(
 				"<span size=\"small\"><b>{0} v{1}.{2}.{3}</b> " + 
-				"<span color=\"blue\">(http://sonance.aaronbock.net/)</span></span>",
+				"<span color=\"blue\">(http://banshee-project.org)</span></span>",
 				StringUtil.UcFirst(selAsm.Name), selAsm.Version.Major, 
 				selAsm.Version.Minor, selAsm.Version.Build
 			);
@@ -126,12 +112,121 @@ namespace Banshee
 
 			VersionStore.SetSortColumnId(0, SortType.Ascending);
 		}
-		
-		void LoadCredits()
+	}
+	
+	public class ScrollBox : DrawingArea
+	{
+		private Pixbuf image;
+		private Pixbuf gradTop;
+		private Pixbuf gradBottom;
+		private int scroll = 0;
+		private int textHeight = 0;
+		private int textWidth = 0;
+		private int offsetX = 20;
+		private Pango.Layout layout;
+		private Pango.Layout shadowLayout;
+		private AssemblyName selAsm;
+
+		private uint TimerHandle;
+	
+		public ScrollBox()
 		{
-			LabelCredits.Markup = 
-			"<b>Lead Developer</b>\n\n" +
-			"\tAaron Bockover &lt;aaron@aaronbock.net&gt;\n\n";
+			image = Gdk.Pixbuf.LoadFromResource("about-footer.png");
+			gradTop = Gdk.Pixbuf.LoadFromResource("about-grad-top.png");
+			gradBottom = Gdk.Pixbuf.LoadFromResource("about-grad-bottom.png");
+			
+			scroll = -image.Height;
+			
+			SetSizeRequest(image.Width, image.Height);
+			
+			Realized += OnRealized;
+			ExposeEvent += OnExposed;
+			
+			TimerHandle = GLib.Timeout.Add(50, new TimeoutHandler(ScrollDown));
+			
+			selAsm = Assembly.GetEntryAssembly().GetName();
+		}
+
+		private string ScrollText 
+		{
+			get {
+				return 
+					String.Format(
+						"<b><big>Version</big>\n" + 
+						"   {0} v{1}.{2}.{3}\n\n" + 
+						"<big>License</big>\n" + 
+						"   Released under the MIT License\n\n" + 
+						"<big>Author</big>\n" + 
+						"   Written by Aaron Bockover\n" + 
+						"   www.banshee-project.org\n" + 
+						"   aaron@aaronbock.net\n\n"  +
+						"<big>Copyright</big>\n" +
+						"   Copyright (C) 2005 Novell\n" + 
+						"   Copyright (C) 2005 Aaron Bockover\n\n",
+						StringUtil.UcFirst(selAsm.Name), selAsm.Version.Major, 
+						selAsm.Version.Minor, selAsm.Version.Build
+					) + 
+					
+					"<big>Development</big>\n" +
+					"    Aaron Bockover (core)\n" +
+					"    James Willcox (ipod-sharp)\n\n" +
+					"<big>Graphics</big>\n" + 
+					"    Garrett LeSage\n" +
+					"    Aaron Bockover\n\n" +
+					"<big>Thanks</big>\n" + 
+					"    Larry Ewing\n" +
+					"    Raphael Slinckx\n" +
+					"    Evan Bockover\n" + 
+					"    Jeff Tickle</b>";	
+			}
+		}
+		
+		private bool ScrollDown()
+		{
+			scroll++;
+			QueueDrawArea(offsetX, 0, textWidth, image.Height);
+			return true;
+		}
+		
+		protected void OnExposed(object o, ExposeEventArgs args)
+		{
+			if(image == null || gradTop == null || gradBottom == null)
+				return;
+			
+			GdkWindow.DrawPixbuf(Style.BackgroundGC(StateType.Normal), 
+				image, 0, 0, 0, 0, -1, -1, RgbDither.Normal,  0,  0);
+			
+			GdkWindow.DrawLayout(Style.TextGC(StateType.Normal), offsetX, 
+				0 - scroll - 1, shadowLayout);
+			
+			GdkWindow.DrawLayout(Style.TextGC(StateType.Normal), offsetX + 2, 
+				0 - scroll + 1, shadowLayout);
+				
+			GdkWindow.DrawLayout(Style.TextGC(StateType.Normal), offsetX + 1, 
+				0 - scroll, layout);
+					
+			GdkWindow.DrawPixbuf(Style.BackgroundGC(StateType.Normal),
+				gradTop, 0, 0, 0, 0, -1, -1, RgbDither.Normal, 0, 0);
+				
+			GdkWindow.DrawPixbuf(Style.BackgroundGC(StateType.Normal),
+				gradBottom, 0, 0, 0, image.Height - gradBottom.Height, 
+				-1, -1, RgbDither.Normal, 0, 0);
+				
+			if(scroll > textHeight)
+				scroll = -scroll;
+		}
+
+		protected void OnRealized (object o, EventArgs args)
+		{
+			layout = new Pango.Layout(PangoContext);
+			layout.SetMarkup("<span color=\"white\">" 
+				+ ScrollText + "</span>");
+			layout.GetPixelSize(out textWidth, out textHeight);
+			
+			shadowLayout = new Pango.Layout(PangoContext);
+			shadowLayout.SetMarkup("<span color=\"black\">" 
+				+ ScrollText + "</span>");
+			shadowLayout.GetPixelSize(out textWidth, out textHeight);
 		}
 	}
 }
