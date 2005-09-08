715a24fea72e20daf97af00e1e811320bf04dd67
Aaron Bockover <abock@src.gnome.org>
2005-09-08 18:46:48 +0000

    Disabled progress, some kind of LTM bug, looking into it

--- src/IpodCore.cs
+++ src/IpodCore.cs
@@ -256,8 +256,8 @@ namespace Banshee
 		
 		public void WrapRun()
 		{
-			statusMessage = String.Format(Catalog.GetString(
-				"Preparing to sync '{0}'"), device.Name);
+			//statusMessage = String.Format(Catalog.GetString(
+			//	"Preparing to sync '{0}'"), device.Name);
 				
 			currentCount = 0;
 			totalCount = 0;
@@ -322,9 +322,9 @@ namespace Banshee
 			         filename = IpodCore.ConvertSongName(libTrack.Uri,
 			             profile.Extension);
 			         
-				    statusMessage = String.Format(
-					Catalog.GetString("Encoding for iPod Usage: {0} - {1}"),
-					song.Artist, song.Title);
+				  //  statusMessage = String.Format(
+				//	Catalog.GetString("Encoding for iPod Usage: {0} - {1}"),
+				//	song.Artist, song.Title);
 					
 					
 			        try {
@@ -360,27 +360,32 @@ namespace Banshee
 				return;
 		    }
 			
-			device.SongDatabase.SaveProgressChanged += OnSaveProgressChanged;
+			//device.SongDatabase.SaveProgressChanged += OnSaveProgressChanged;
 
 			try {
+			    //statusMessage = Catalog.GetString("Synchronizing iPod...");
+			    //currentCount = 0;
+			    //totalCount = 0;
 				device.SongDatabase.Save();
 			} catch(Exception e) {
 				Core.Log.Push(LogEntryType.UserError, 
 					Catalog.GetString("Could not sync iPod"),
 					e.Message);
 			}
+			
 
-			device.SongDatabase.SaveProgressChanged -= OnSaveProgressChanged;
+			//device.SongDatabase.SaveProgressChanged -= OnSaveProgressChanged;
 		} 
 		
-		private void OnSaveProgressChanged(SongDatabase db, Song song, 
+		/*private void OnSaveProgressChanged(SongDatabase db, Song song, 
 			double currentPercent, int completed, int total)
 		{
+			
 			currentCount = completed;
 			totalCount = total;
 			statusMessage = String.Format(Catalog.GetString(
-				"Copying {0} - {1}"), song.Artist, song.Title); 
-		}
+				"Copying {0} - {1}"), song.Artist, song.Title);
+		}*/
 		
 		private void OnEncodeProgress(object o, FileEncoderProgressArgs args)
 		{
