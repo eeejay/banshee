4161daa1e537227eea60b247955cfedea433a087
Aaron Bockover <abock@src.gnome.org>
2005-10-21 05:54:56 +0000

    Add more DBus Methods

--- src/DBusIPC.cs
+++ src/DBusIPC.cs
@@ -32,91 +32,185 @@ using DBus;
 
 namespace Banshee
 {
-	public class DBusServer
-	{
-		private static DBusServer instance = null;
-		
-		private Service service;
-			
-		public static DBusServer Instance
-		{
-			get {
-				if(instance == null)
-					instance = new DBusServer();
-				return instance;
-			}
-		}
-		
-		public DBusServer()
-		{
+    public class DBusServer
+    {
+        private static DBusServer instance = null;
+        
+        private Service service;
+            
+        public static DBusServer Instance
+        {
+            get {
+                if(instance == null)
+                    instance = new DBusServer();
+                return instance;
+            }
+        }
+        
+        public DBusServer()
+        {
             service = new Service(Bus.GetSessionBus(), "org.gnome.Banshee");
-		}
-		
-		public void RegisterObject(object o, string path)
-		{
-			service.RegisterObject(o, path);
-		}
-		
-		public void UnregisterObject(object o)
-		{
-			service.UnregisterObject(o);
-		}
-	}
-	
-	[Interface("org.gnome.Banshee.Core")]
-	public class BansheeCore
-	{
-		private Gtk.Window mainWindow;
-		private PlayerUI PlayerUI;
-		
-		public static BansheeCore FindInstance()
-		{
-			Connection connection = Bus.GetSessionBus();
-			Service service = Service.Get(connection, "org.gnome.Banshee");		
-			return (BansheeCore)service.GetObject(typeof(BansheeCore), 
-				"/org/gnome/Banshee/Core");
-		}
-		
-		public BansheeCore(Gtk.Window mainWindow, PlayerUI ui)
-		{
-			this.mainWindow = mainWindow;
-			this.PlayerUI = ui;
-		}
-		
-		[Method]
-		public virtual void PresentWindow()
-		{
-			if(mainWindow != null)
-				mainWindow.Present();
-		}
-		
-		[Method]
-		public virtual void TogglePlaying()
-		{
-			if(PlayerUI != null)
-				PlayerUI.TogglePlaying();
-		}
-		
-		[Method]
-		public virtual void Next()
-		{
-			if(PlayerUI != null)
-				PlayerUI.Next();
-		}
-		
-		[Method]
-		public virtual void Previous()
-		{
-			if(PlayerUI != null)
-				PlayerUI.Previous();
-		}
+        }
+        
+        public void RegisterObject(object o, string path)
+        {
+            service.RegisterObject(o, path);
+        }
+        
+        public void UnregisterObject(object o)
+        {
+            service.UnregisterObject(o);
+        }
+    }
+    
+    [Interface("org.gnome.Banshee.Core")]
+    public class BansheeCore
+    {
+        private Gtk.Window mainWindow;
+        private PlayerUI PlayerUI;
+        private Core core;
+        
+        public static BansheeCore FindInstance()
+        {
+            Connection connection = Bus.GetSessionBus();
+            Service service = Service.Get(connection, "org.gnome.Banshee");        
+            return (BansheeCore)service.GetObject(typeof(BansheeCore), 
+                "/org/gnome/Banshee/Core");
+        }
+        
+        public BansheeCore(Gtk.Window mainWindow, PlayerUI ui, Core core)
+        {
+            this.mainWindow = mainWindow;
+            this.PlayerUI = ui;
+            this.core = core;
+        }
+        
+        [Method]
+        public virtual void PresentWindow()
+        {
+            if(mainWindow != null)
+                mainWindow.Present();
+        }
+        
+        [Method]
+        public virtual void ShowWindow()
+        {
+            if(mainWindow != null)
+                mainWindow.Show();
+        }
+        
+        [Method]
+        public virtual void HideWindow()
+        {
+            if(mainWindow != null)
+                mainWindow.Hide();
+        }
+        
+        [Method]
+        public virtual void TogglePlaying()
+        {
+            if(PlayerUI != null)
+                PlayerUI.TogglePlaying();
+        }
+        
+        [Method]
+        public virtual void Play()
+        {
+            if(PlayerUI == null) {
+                return;
+            }
+            
+            if(PlayerUI != null && !HaveTrack) {
+                PlayerUI.TogglePlaying();
+            }
+            
+            if(!core.Player.Playing) {
+                PlayerUI.TogglePlaying();
+            }
+        }
+        
+        [Method]
+        public virtual void Pause()
+        {
+            if(HaveTrack && core.Player.Playing) {
+                PlayerUI.TogglePlaying();
+            }
+        }
+        
+        [Method]
+        public virtual void Next()
+        {
+            if(PlayerUI != null)
+                PlayerUI.Next();
+        }
+        
+        [Method]
+        public virtual void Previous()
+        {
+            if(PlayerUI != null)
+                PlayerUI.Previous();
+        }
 
-		[Method]
-		public virtual void SelectAudioCd(string device)
-		{
-			if(PlayerUI != null)
-				PlayerUI.SelectAudioCd(device);
-		}
-	}
+        [Method]
+        public virtual void SelectAudioCd(string device)
+        {
+            if(PlayerUI != null)
+                PlayerUI.SelectAudioCd(device);
+        }
+        
+        private bool HaveTrack {
+            get {
+                return PlayerUI != null && PlayerUI.ActiveTrackInfo != null;
+            }
+        }
+        
+        [Method]
+        public virtual string GetPlayingArtist()
+        {
+            return HaveTrack ? PlayerUI.ActiveTrackInfo.Artist : null;
+        }
+        
+        [Method]
+        public virtual string GetPlayingAlbum()
+        {
+            return HaveTrack ? PlayerUI.ActiveTrackInfo.Album : null;
+        }
+        
+        [Method]
+        public virtual string GetPlayingTitle()
+        {
+            return HaveTrack ? PlayerUI.ActiveTrackInfo.Title : null;
+        }
+        
+        [Method]
+        public virtual string GetPlayingGenre()
+        {
+            return HaveTrack ? PlayerUI.ActiveTrackInfo.Genre : null;
+        }
+        
+        [Method]
+        public virtual string GetPlayingUri()
+        {
+            return HaveTrack ? PlayerUI.ActiveTrackInfo.Uri.AbsoluteUri : null;
+        }
+        
+        [Method]
+        public virtual int GetPlayingDuration()
+        {
+            return HaveTrack ? (int)core.Player.Length : -1;
+        }
+        
+        [Method]
+        public virtual int GetPlayingPosition()
+        {
+            return HaveTrack ? (int)core.Player.Position : -1;
+        }
+        
+        [Method]
+        public virtual int GetPlayingStatus()
+        {
+            return core.Player.Playing ? 1 : (core.Player.Loaded ? 0 : -1);
+        }
+    }
 }
 
