6e30a6a0c53b1947a1bf7c8125eb23ae535e3033
Aaron Bockover <abock@src.gnome.org>
2005-10-22 16:57:45 +0000

    Cleaned up, still hosed

--- src/IpodCore.cs
+++ src/IpodCore.cs
@@ -46,77 +46,75 @@ namespace Banshee
         public Device Device;
     }
 
-	public class IpodCore : IDisposable
-	{
-		private static IpodCore instance;
-		private DeviceEventListener listener;
-		private Hashtable devices;
-		
-		private static string [] validSongExtensions = 
-		  {"mp3", "aac", "mp4", "m4a", "m4p"};
-		
-		public event IpodDeviceAddedHandler DeviceAdded;
-		public event IpodDeviceRemovedHandler DeviceRemoved;
-	
-		public IpodCore()
-		{
-			listener = new DeviceEventListener();
-			listener.DeviceAdded += OnDeviceAdded;
-			listener.DeviceRemoved += OnDeviceRemoved;
-			
-			devices = new Hashtable();
-			
-			foreach(Device device in Device.ListDevices()) {
-				devices[device.VolumeId] = device;
-			}
-		}
-		
-		public void Dispose()
-		{
-			
-		}
-		
-		private void OnDeviceAdded(object o, DeviceAddedArgs args)
-		{
-			if(devices[args.Udi] == null) {
-				devices[args.Udi] = new Device(args.Udi);
-				IpodDeviceAddedHandler handler = DeviceAdded;
-				if(handler != null) {
-				    IpodDeviceArgs dargs = new IpodDeviceArgs();
-				    dargs.Device = devices[args.Udi] as Device;
-				    handler(this, dargs);
-			    }
-			}
-		}
-		
-		private void OnDeviceRemoved(object o, DeviceRemovedArgs args)
-		{
-			if(devices[args.Udi] != null) {
-			    Device device = devices[args.Udi] as Device; 
-				devices.Remove(args.Udi);
-				IpodDeviceRemovedHandler handler = DeviceRemoved;
-				if(handler != null) {
-				    IpodDeviceArgs dargs = new IpodDeviceArgs();
-				    dargs.Device = device;
-				    handler(this, dargs);
-			    }
-			}
-		}
-		
-		public void ListAll()
-		{
-			//foreach(Device device in Device.ListDevices())
-			//	device.Debug();
-		}
-		
-		public Device [] Devices
-		{
-			get {
-				ArrayList list = new ArrayList(devices.Values);
-				return list.ToArray(typeof(Device)) as Device [];
-			}
-		}
-		
+    public class IpodCore : IDisposable
+    {
+        private static IpodCore instance;
+        private DeviceEventListener listener;
+        private Hashtable devices;
+        
+        private static string [] validSongExtensions = {"mp3", "aac", "mp4", "m4a", "m4p"};
+        
+        public event IpodDeviceAddedHandler DeviceAdded;
+        public event IpodDeviceRemovedHandler DeviceRemoved;
+    
+        public IpodCore()
+        {
+            listener = new DeviceEventListener();
+            listener.DeviceAdded += OnDeviceAdded;
+            listener.DeviceRemoved += OnDeviceRemoved;
+            
+            devices = new Hashtable();
+            
+            foreach(Device device in Device.ListDevices()) {
+                devices[device.VolumeId] = device;
+            }
+        }
+        
+        public void Dispose()
+        {
+        }
+        
+        private void OnDeviceAdded(object o, DeviceAddedArgs args)
+        {
+            if(devices[args.Udi] == null) {
+                devices[args.Udi] = new Device(args.Udi);
+                IpodDeviceAddedHandler handler = DeviceAdded;
+                if(handler != null) {
+                    IpodDeviceArgs dargs = new IpodDeviceArgs();
+                    dargs.Device = devices[args.Udi] as Device;
+                    handler(this, dargs);
+                }
+            }
+        }
+        
+        private void OnDeviceRemoved(object o, DeviceRemovedArgs args)
+        {
+            if(devices[args.Udi] != null) {
+                Device device = devices[args.Udi] as Device; 
+                devices.Remove(args.Udi);
+                IpodDeviceRemovedHandler handler = DeviceRemoved;
+                if(handler != null) {
+                    IpodDeviceArgs dargs = new IpodDeviceArgs();
+                    dargs.Device = device;
+                    handler(this, dargs);
+                }
+            }
+        }
+        
+        public void ListAll()
+        {
+            //foreach(Device device in Device.ListDevices())
+            //    device.Debug();
+        }
+        
+        public Device [] Devices
+        {
+            get {
+                ArrayList list = new ArrayList(devices.Values);
+                return list.ToArray(typeof(Device)) as Device [];
+            }
+        }
+        
         public static bool ValidSongFormat(string filename)
         {
             string ext = Path.GetExtension(filename).ToLower().Trim();
@@ -142,8 +140,8 @@ namespace Banshee
             string file = Path.GetFileNameWithoutExtension(filename);
             
             foreach(string vext in validSongExtensions) {
-                string newfile = dir + Path.DirectorySeparatorChar +  
-                    ".banshee-ipod-" + file + "." + vext;
+                string newfile = dir + Path.DirectorySeparatorChar + ".banshee-ipod-" 
+                    + file + "." + vext;
                 
                 if(File.Exists(newfile))
                     return newfile;
@@ -167,11 +165,10 @@ namespace Banshee
             string dir = Path.GetDirectoryName(path);
             string file = Path.GetFileNameWithoutExtension(filename);
             
-            return dir + Path.DirectorySeparatorChar + 
-                ".banshee-ipod-" + file + "." + newext;
+            return dir + Path.DirectorySeparatorChar + ".banshee-ipod-" + file + "." + newext;
         }
-	}
-	
+    }
+    
     public class IpodSync
     {
         private PipelineProfile encodeProfile;
@@ -190,8 +187,7 @@ namespace Banshee
         {
             this.device = device;
             
-            encodeProfile = PipelineProfile.GetConfiguredProfile(
-                "Ipod", "mp3,aac,mp4,m4a,m4p");
+            encodeProfile = PipelineProfile.GetConfiguredProfile("Ipod", "mp3,aac,mp4,m4a,m4p");
                 
            // progress = new ProgressDialog(null);
            // progress.SongDatabase = device.SongDatabase;
@@ -232,8 +228,9 @@ namespace Banshee
             }
 
             foreach(Song song in songs) {
-                if(IpodMisc.TrackCompare(libTrack, song))
+                if(IpodMisc.TrackCompare(libTrack, song)) {
                     return true;
+                }
             }
 
             return false;
@@ -242,8 +239,9 @@ namespace Banshee
         private bool ExistsInLibrary(Song song)
         {
             foreach(LibraryTrackInfo libTrack in Core.Library.Tracks.Values) {
-                if(IpodMisc.TrackCompare(libTrack, song))
+                if(IpodMisc.TrackCompare(libTrack, song)) {
                     return true;
+                }
             }
 
             return false;
@@ -257,12 +255,14 @@ namespace Banshee
             Song [] ipodSongs = device.SongDatabase.Songs;
             
             ICollection collection = updateTracks;
-            if(collection == null)
+            if(collection == null) {
                 collection = Core.Library.Tracks.Values;
-                
+            }
+            
             foreach(TrackInfo libTrack in collection) {                    
-                if(ExistsOnIpod(ipodSongs, libTrack) || libTrack.Uri == null)
+                if(ExistsOnIpod(ipodSongs, libTrack) || libTrack.Uri == null) {
                     continue;
+                }
 
                 Song song = null;
 
@@ -274,8 +274,9 @@ namespace Banshee
                     continue;
                 }
 
-                if(song == null)
+                if(song == null) {
                     continue;
+                }
 
                 string filename = IpodCore.GetIpodSong(libTrack.Uri);
                 
@@ -331,8 +332,9 @@ namespace Banshee
             
             if(updateTracks == null) {
                 foreach(Song song in device.SongDatabase.Songs) {
-                    if(ExistsInLibrary(song))
+                    if(ExistsInLibrary(song)) {
                         continue;
+                    }
 
                     device.SongDatabase.RemoveSong(song);
                     doUpdate = true;
@@ -351,112 +353,107 @@ namespace Banshee
                 }
             }
             
-            if(!doUpdate)
+            if(!doUpdate) {
                 return;
+            }
                 
-            Thread thread = new Thread(new ThreadStart(ThreadedSave));
-            thread.Start();
+            ThreadPool.QueueUserWorkItem(ThreadedSave);
         }
         
-        private void ThreadedSave()
+        private void ThreadedSave(object o)
         {
-           try {
-                device.SongDatabase.Save();
-            
-           } catch(Exception e) {
-            Console.WriteLine(e);
-           }
+            device.SongDatabase.Save();
         }    
     }
     
-	public class IpodMisc
-	{
-		public static Song TrackInfoToSong(Device device, 
-			LibraryTrackInfo libTrack)
-		{
-			Song song = device.SongDatabase.CreateSong();
-			
-			if(libTrack.Album != null)
-				song.Album = libTrack.Album;
-			
-			if(libTrack.Artist != null)
-				song.Artist = libTrack.Artist;
-			
-			if(libTrack.Title != null)
-				song.Title = libTrack.Title;
-			
-			if(libTrack.Genre != null)
-				song.Genre = libTrack.Genre;
-			
-			song.Length = (int)(libTrack.Duration * 1000);
-			song.TrackNumber = (int)libTrack.TrackNumber;
-			song.TotalTracks = (int)libTrack.TrackCount;
-			song.Year = (int)libTrack.Year;
-			//song.LastPlayed = libTrack.LastPlayed;
-			song.LastPlayed = DateTime.MinValue;
-			
-			switch(libTrack.Rating) {
-			    case 1: song.Rating = SongRating.Zero; break;
-			    case 2: song.Rating = SongRating.Two; break;
-			    case 3: song.Rating = SongRating.Three; break;
-			    case 4: song.Rating = SongRating.Four; break;
-			    case 5: song.Rating = SongRating.Five; break;
-			    default: song.Rating = SongRating.Zero; break;
-			}
-			
-			if(song.Artist == null)
-				song.Artist = String.Empty;
+    public class IpodMisc
+    {
+        public static Song TrackInfoToSong(Device device, 
+            LibraryTrackInfo libTrack)
+        {
+            Song song = device.SongDatabase.CreateSong();
+            
+            if(libTrack.Album != null)
+                song.Album = libTrack.Album;
+            
+            if(libTrack.Artist != null)
+                song.Artist = libTrack.Artist;
+            
+            if(libTrack.Title != null)
+                song.Title = libTrack.Title;
+            
+            if(libTrack.Genre != null)
+                song.Genre = libTrack.Genre;
+            
+            song.Length = (int)(libTrack.Duration * 1000);
+            song.TrackNumber = (int)libTrack.TrackNumber;
+            song.TotalTracks = (int)libTrack.TrackCount;
+            song.Year = (int)libTrack.Year;
+            //song.LastPlayed = libTrack.LastPlayed;
+            song.LastPlayed = DateTime.MinValue;
+            
+            switch(libTrack.Rating) {
+                case 1: song.Rating = SongRating.Zero; break;
+                case 2: song.Rating = SongRating.Two; break;
+                case 3: song.Rating = SongRating.Three; break;
+                case 4: song.Rating = SongRating.Four; break;
+                case 5: song.Rating = SongRating.Five; break;
+                default: song.Rating = SongRating.Zero; break;
+            }
+            
+            if(song.Artist == null)
+                song.Artist = String.Empty;
 
-			if(song.Album == null)
-				song.Album = String.Empty;
+            if(song.Album == null)
+                song.Album = String.Empty;
 
-			if(song.Title == null)
-				song.Title = String.Empty;
+            if(song.Title == null)
+                song.Title = String.Empty;
 
-			if(song.Genre == null)
-				song.Genre = String.Empty;
-				
-			return song;
-		}
-		
-		private static string ToLower (string str)
-		{
-			if (str == null)
-				return null;
-			else
-				return str.ToLower ();
-		}
-		
-		public static bool TrackCompare(TrackInfo libTrack, Song song)
-		{
-			return ToLower(song.Title) == ToLower(libTrack.Title) && 
-				ToLower(song.Album) == ToLower(libTrack.Album) &&
-				ToLower(song.Artist) == ToLower(libTrack.Artist) &&
-				song.Year == libTrack.Year &&
-				song.TrackNumber == libTrack.TrackNumber;
-		}
-		
-		public static string GetDiskUsageString(Device device)
-		{
-			// Translators: iPod disk usage. Each {N} is something like "100 MB"
-			return String.Format(
-				Catalog.GetString("{0} of {1}"),
-				Utilities.BytesToString(device.VolumeUsed),
-				Utilities.BytesToString(device.VolumeSize));
-		}
+            if(song.Genre == null)
+                song.Genre = String.Empty;
+                
+            return song;
+        }
+        
+        private static string ToLower (string str)
+        {
+            if(str == null) {
+                return null;
+            } else {
+                return str.ToLower ();
+            }
+        }
+        
+        public static bool TrackCompare(TrackInfo libTrack, Song song)
+        {
+            return ToLower(song.Title) == ToLower(libTrack.Title) && 
+                ToLower(song.Album) == ToLower(libTrack.Album) &&
+                ToLower(song.Artist) == ToLower(libTrack.Artist) &&
+                song.Year == libTrack.Year &&
+                song.TrackNumber == libTrack.TrackNumber;
+        }
+        
+        public static string GetDiskUsageString(Device device)
+        {
+            // Translators: iPod disk usage. Each {N} is something like "100 MB"
+            return String.Format(
+                Catalog.GetString("{0} of {1}"),
+                Utilities.BytesToString(device.VolumeUsed),
+                Utilities.BytesToString(device.VolumeSize));
+        }
 
-		public static string GetDiskAvailableString(Device device)
-		{
-			// Translators: iPod disk usage. {0} is something like "100 MB"
-			return String.Format(
-				Catalog.GetString("({0} Remaining)"),
-				Utilities.BytesToString(device.VolumeAvailable));
-		}
-	   
-	    public static double GetDiskUsageFraction(Device device)
-	    {
-	       return (double)device.VolumeUsed /
-					(double)device.VolumeSize;
-	    }
-	}
+        public static string GetDiskAvailableString(Device device)
+        {
+            // Translators: iPod disk usage. {0} is something like "100 MB"
+            return String.Format(
+                Catalog.GetString("({0} Remaining)"),
+                Utilities.BytesToString(device.VolumeAvailable));
+        }
+       
+        public static double GetDiskUsageFraction(Device device)
+        {
+           return (double)device.VolumeUsed / (double)device.VolumeSize;
+        }
+    }
 }
