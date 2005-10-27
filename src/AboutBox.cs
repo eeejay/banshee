e27fe8960d0ce01a82cd7f41746e80ef839ae0c4
Aaron Bockover <abock@src.gnome.org>
2005-10-27 02:04:38 +0000

    Use the GTK About Dialog

--- src/AboutBox.cs
+++ src/AboutBox.cs
@@ -31,8 +31,164 @@ using System;
 using Gtk;
 using Mono.Unix;
 
+using System.Runtime.InteropServices;
+
 namespace Banshee
 {
+    public class AboutDialog : Gtk.Dialog
+    {
+        protected AboutDialog(IntPtr raw) : base(raw) 
+        {
+        }
+        
+        public AboutDialog() : base(IntPtr.Zero)
+        {
+            CreateNativeObject(new string[0], new GLib.Value[0]);
+        }
+        
+        [DllImport("libgtk-win32-2.0-0.dll")]
+        static extern IntPtr gtk_about_dialog_get_type();
+        
+        public static new GLib.GType GType {
+            get {
+                return new GLib.GType(gtk_about_dialog_get_type());
+            }
+        }
+        
+        [DllImport("libgtk-win32-2.0-0.dll")]
+        static extern IntPtr gtk_about_dialog_set_name(IntPtr raw, IntPtr name);
+        
+        public new string Name {
+            set {
+                IntPtr raw = GLib.Marshaller.StringToPtrGStrdup(value);
+                gtk_about_dialog_set_name(Handle, raw);
+                GLib.Marshaller.Free(raw);
+            }
+        }
+        
+        [DllImport("libgtk-win32-2.0-0.dll")]
+        static extern IntPtr gtk_about_dialog_set_version(IntPtr raw, IntPtr version);
+        
+        public string Version {
+            set {
+                IntPtr raw = GLib.Marshaller.StringToPtrGStrdup(value);
+                gtk_about_dialog_set_version(Handle, raw);
+                GLib.Marshaller.Free(raw);
+            }
+        }
+        
+        [DllImport("libgtk-win32-2.0-0.dll")]
+        static extern IntPtr gtk_about_dialog_set_copyright(IntPtr raw, IntPtr copyright);
+        
+        public string Copyright {   
+            set {
+                IntPtr raw = GLib.Marshaller.StringToPtrGStrdup(value);
+                gtk_about_dialog_set_copyright(Handle, raw);
+                GLib.Marshaller.Free(raw);
+            }
+        }
+        
+        [DllImport("libgtk-win32-2.0-0.dll")]
+        static extern IntPtr gtk_about_dialog_set_comments(IntPtr raw, IntPtr comments);
+        
+        public string Comments {   
+            set {
+                IntPtr raw = GLib.Marshaller.StringToPtrGStrdup(value);
+                gtk_about_dialog_set_comments(Handle, raw);
+                GLib.Marshaller.Free(raw);
+            }
+        }
+        
+        [DllImport("libgtk-win32-2.0-0.dll")]
+        static extern IntPtr gtk_about_dialog_set_license(IntPtr raw, IntPtr license);
+        
+        public string License {   
+            set {
+                IntPtr raw = GLib.Marshaller.StringToPtrGStrdup(value);
+                gtk_about_dialog_set_license(Handle, raw);
+                GLib.Marshaller.Free(raw);
+            }
+        }
+        
+        [DllImport("libgtk-win32-2.0-0.dll")]
+        static extern IntPtr gtk_about_dialog_set_wrap_license(IntPtr raw, bool wrap);
+        
+        public bool WrapLicense {   
+            set {
+                gtk_about_dialog_set_wrap_license(Handle, value);
+            }
+        }
+        
+        [DllImport("libgtk-win32-2.0-0.dll")]
+        static extern IntPtr gtk_about_dialog_set_website(IntPtr raw, IntPtr website);
+        
+        public string Website {   
+            set {
+                IntPtr raw = GLib.Marshaller.StringToPtrGStrdup(value);
+                gtk_about_dialog_set_website(Handle, raw);
+                GLib.Marshaller.Free(raw);
+            }
+        }
+        
+        [DllImport("libgtk-win32-2.0-0.dll")]
+        static extern IntPtr gtk_about_dialog_set_website_label(IntPtr raw, IntPtr website_label);
+        
+        public string WebsiteLabel {   
+            set {
+                IntPtr raw = GLib.Marshaller.StringToPtrGStrdup(value);
+                gtk_about_dialog_set_website_label(Handle, raw);
+                GLib.Marshaller.Free(raw);
+            }
+        }
+        
+        [DllImport("libgtk-win32-2.0-0.dll")]
+        static extern IntPtr gtk_about_dialog_set_authors(IntPtr raw, string [] authors);
+        
+        public string [] Authors {
+            set {
+               gtk_about_dialog_set_authors(Handle, value);
+            }
+        }
+        
+        [DllImport("libgtk-win32-2.0-0.dll")]
+        static extern IntPtr gtk_about_dialog_set_artists(IntPtr raw, string [] artists);
+        
+        public string [] Artists {
+            set {
+               gtk_about_dialog_set_artists(Handle, value);
+            }
+        }
+        
+        [DllImport("libgtk-win32-2.0-0.dll")]
+        static extern IntPtr gtk_about_dialog_set_documenters(IntPtr raw, string [] documenters);
+        
+        public string [] Documenters {
+            set {
+               gtk_about_dialog_set_documenters(Handle, value);
+            }
+        }
+        
+        [DllImport("libgtk-win32-2.0-0.dll")]
+        static extern IntPtr gtk_about_dialog_set_translator_credits(IntPtr raw, IntPtr credits);
+        
+        public string TranslatorCredits {
+            set {
+                IntPtr raw = GLib.Marshaller.StringToPtrGStrdup(value);
+                gtk_about_dialog_set_translator_credits(Handle, raw);
+                GLib.Marshaller.Free(raw);
+            }
+        }
+        
+        [DllImport("libgtk-win32-2.0-0.dll")]
+        static extern IntPtr gtk_about_dialog_set_logo(IntPtr raw, IntPtr logo);
+        
+        public Gdk.Pixbuf Logo {
+            set {
+                gtk_about_dialog_set_logo(Handle, value.Raw);
+            }
+        }
+    }
+
     public class AboutBox
     {
         private static string [] Authors = {
@@ -42,7 +198,14 @@ namespace Banshee
             "James Wilcox",
             "Chris Lahey",
             "Ben Maurer",
-            "Larry Ewing"
+            "Larry Ewing",
+            "Miguel de Icaza"
+        };
+        
+        private static string [] Artists = {
+            "Garrett LeSage",
+            "Jakub Steiner",
+            "Ryan Collier"
         };
     
         private Translator [] Translators = {
@@ -83,41 +246,40 @@ namespace Banshee
             
             public static string ToString(Translator [] translators)
             {
-                string ret = String.Empty;
-
+                string str = String.Empty;
                 Array.Sort(translators);
                 
-                foreach(Translator translator in translators) {
-                    ret += translator + "\n";
+                for(int i = 0; i < translators.Length; i++) {
+                    str += translators[i].ToString() + "\n";
                 }
                 
-                return ret;
+                return str;
             }
         }
     
         public AboutBox()
         {
             Array.Sort(Authors);
+            Array.Sort(Artists);
             
-            Gnome.About about = new Gnome.About(
-                "Banshee", 
-                ConfigureDefines.VERSION,
-                Catalog.GetString(
-                    "Copyright 2005 Novell, Inc.\n" + 
-                    "Copyright 2005 Aaron Bockover"),
-                String.Format(Catalog.GetString(
-                    "Music Management and Playback for Gnome.\n" + 
-                    "Released under the MIT license.\n" + 
-                    "See the Banshee Wiki at {0}"), 
-                    "www.banshee-project.org"),
-                Authors,
-                null,
-                Translator.ToString(Translators),
-                Gdk.Pixbuf.LoadFromResource("banshee-logo.png")
-            );
-            
-            about.Icon = ThemeIcons.WindowManager;
-            about.Run();
+            AboutDialog about_dialog = new AboutDialog();
+            about_dialog.Name = "Banshee";
+            about_dialog.Version = ConfigureDefines.VERSION;
+            about_dialog.Copyright = Catalog.GetString(
+                "Copyright 2005 Novell, Inc.\n" + 
+                "Copyright 2005 Aaron Bockover");
+            about_dialog.Comments = Catalog.GetString("Music Management and Playback for Gnome.");
+            about_dialog.Website = "http://banshee-project.org/";
+            about_dialog.WebsiteLabel = "Banshee Wiki";
+            about_dialog.Authors = Authors;
+            about_dialog.TranslatorCredits = Translator.ToString(Translators);
+            about_dialog.Artists = Artists;
+            about_dialog.Icon = ThemeIcons.WindowManager;
+            about_dialog.Logo = Gdk.Pixbuf.LoadFromResource("banshee-logo.png");
+            about_dialog.License = Resource.GetFileContents("COPYING");
+            about_dialog.WrapLicense = true;
+            about_dialog.Run();
+            about_dialog.Destroy();
         }
     }
 }
