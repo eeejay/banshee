5ff194cca7bf7b12d7e95cb606b8e5f7a3fc2734
Aaron Bockover <abock@src.gnome.org>
2005-11-13 04:21:08 +0000

    little changes

--- vacuum.sh
+++ vacuum.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-if [ ! -f Makefile ]; then
-	exit 1;
+if [ -f Makefile ]; then
+	make maintainer-clean
 fi
 
-make maintainer-clean
 rm -f compile INSTALL config.h.in aclocal.m4 ltmain.sh Makefile.in depcomp missing install-sh configure config.sub config.guess mkinstalldirs
 
