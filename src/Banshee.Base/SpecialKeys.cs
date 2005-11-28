6f60d3b39f9246d018d217a93807b0016ae3b0d4
Aaron Bockover <aaron@aaronbock.net>
2005-11-28 07:44:31 +0000

    minor cleanup/trim-down
    2005-11-28  Aaron Bockover  <aaron@aaronbock.net>
    
        * src/Banshee.Base/SpecialKeys.cs: minor cleanup/trim-down

--- src/Banshee.Base/SpecialKeys.cs
+++ src/Banshee.Base/SpecialKeys.cs
@@ -183,8 +183,8 @@ namespace Banshee.Base
             Shift   = 1 << 0,
             Lock	    = 1 << 1,
             Control = 1 << 2,
-            Mod1	    = 1 << 3,
-            Mod2	    = 1 << 4,
+            Mod1    = 1 << 3,
+            Mod2    = 1 << 4,
             Mod3    = 1 << 5,
             Mod4    = 1 << 6,
             Mod5	    = 1 << 7
