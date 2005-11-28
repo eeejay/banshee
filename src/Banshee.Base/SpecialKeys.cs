0be6bd3cb513ad03d13bca7b753c044f4232b90e
Aaron Bockover <aaron@aaronbock.net>
2005-11-28 07:45:20 +0000

    minor cleanup/trim-down
    2005-11-28  Aaron Bockover  <aaron@aaronbock.net>
    
        * src/Banshee.Base/SpecialKeys.cs: minor cleanup/trim-down

--- src/Banshee.Base/SpecialKeys.cs
+++ src/Banshee.Base/SpecialKeys.cs
@@ -181,13 +181,13 @@ namespace Banshee.Base
         private enum XModMask {
             None    = 0,
             Shift   = 1 << 0,
-            Lock	    = 1 << 1,
+            Lock    = 1 << 1,
             Control = 1 << 2,
             Mod1    = 1 << 3,
             Mod2    = 1 << 4,
             Mod3    = 1 << 5,
             Mod4    = 1 << 6,
-            Mod5	    = 1 << 7
+            Mod5    = 1 << 7
         }
         
         private enum XGrabMode {
