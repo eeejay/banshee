595ea77ad93f4c5e648a30cfbd84ae52673ba7fc
Patrick van Staveren <trick@vanstaveren.us>
2006-07-18 06:10:18 +0000

    sync'd device list with libgphoto
    2006-07-18  Patrick van Staveren  <trick@vanstaveren.us>
    
    	* src/Banshee.Dap/Mtp/MtpDeviceId.cs: sync'd device list with libgphoto

--- src/Banshee.Dap/Mtp/MtpDeviceId.cs
+++ src/Banshee.Dap/Mtp/MtpDeviceId.cs
@@ -43,30 +43,35 @@ namespace Banshee.Dap.Mtp
 
                          Full Device Name                               Display/Short Name      Vendor ID   Product ID */
             new DeviceId("iRiver T10 (alternative)",                    "iRiver T10",           0x4102,     0x1113),
+            new DeviceId("iRiver T20 FM",                               "iRiver T20 FM",        0x4102,     0x1114),
             new DeviceId("iRiver U10",                                  "iRiver U10",           0x4102,     0x1116),
             new DeviceId("iRiver T10",                                  "iRiver T10",           0x4102,     0x1117),
             new DeviceId("iRiver T20",                                  "iRiver T20",           0x4102,     0x1118),
             new DeviceId("iRiver T30",                                  "iRiver T30",           0x4102,     0x1119),
             new DeviceId("iRiver H10",                                  "iRiver H10",           0x4102,     0x2102),
-            /* iRiver H30? Anyone? */
             new DeviceId("iRiver Portable Media Center",                "iRiver PMC",           0x1006,     0x4002),
             new DeviceId("iRiver Portable Media Center",                "iRiver PMC",           0x1006,     0x4003),
             new DeviceId("Phillips HDD6320",                            "HDD6320",              0x0471,     0x01eb),
+            new DeviceId("Phillips HDD6130/17",                         "HDD6130/17",           0x0471,     0x014c),
             new DeviceId("Creative Zen Vision",                         "Zen Vision",           0x041e,     0x411f),
             new DeviceId("Creative Portable Media Center",              "Media Center",         0x041e,     0x4123),
             new DeviceId("Creative Zen Xtra",                           "Zen Xtra",             0x041e,     0x4128),
             new DeviceId("Second Generation Dell DJ",                   "Dell DJ",              0x041e,     0x412f),
             new DeviceId("Creative Zen Micro",                          "Zen Micro",            0x041e,     0x4130),
             new DeviceId("Creative Zen Touch",                          "Zen Touch",            0x041e,     0x4131),
+            new DeviceId("Dell Pocket DJ",                              "Pocket DJ",            0x041e,     0x4132),
             new DeviceId("Creative Zen Sleek",                          "Zen Sleek",            0x041e,     0x4137),
             new DeviceId("Creative Zen MicroPhoto",                     "Zen MicroPhoto",       0x041e,     0x413c),
             new DeviceId("Creative Zen Sleek Photo",                    "Zen Sleek Photo",      0x041e,     0x413d),
             new DeviceId("Creative Zen Vision:M",                       "Zen Vision:M",         0x041e,     0x413e),
             new DeviceId("Samsung YP-T7J",                              "Samsung YP-T7J",       0x04e8,     0x5047),
             new DeviceId("Samsung YH-999 Portable Media Center",        "Samsung YH-999",       0x04e8,     0x5a0f),
+            new DeviceId("Samsung YH-925",                              "Samsung YH-925",       0x04e8,     0x502f),
             new DeviceId("Dell DJ Ditty",                               "DJ Ditty",             0x413c,     0x4500),
             new DeviceId("Toshiba Gigabeat",                            "Gigabeat",             0x0930,     0x000c),
-            new DeviceId("Intel Bandon Portable Media Center",          "Intel Bandon",         0x045e,     0x00c9)
+            new DeviceId("JVC Aleno XA-HD500",                          "JVC Aleno",            0x04f1,     0x6105),
+            new DeviceId("Intel Bandon Portable Media Center",          "Intel Bandon",         0x045e,     0x00c9),
+            new DeviceId("Sandisk Sansa e200",                          "Sansa e200",           0x0781,     0x7420)
         };
 
         public static DeviceId [] ListAll {
