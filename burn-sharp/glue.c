d62b246c9bf8be8bf9a5a50b7c98a0e35329164f
Aaron Bockover <abock@gnome.org>
2006-07-20 01:16:46 +0000

    Patch from Ed Catmur (BGO #346501)
    2006-07-19  Aaron Bockover  <abock@gnome.org>
    
        Patch from Ed Catmur (BGO #346501)
    
        * configure.ac: Check for libnautilus-burn 2.15.x and set compat
        defines for 2.12.x, 2.14.x, and 2.15.x
    
        * burn-sharp/glue.c: Stay compatible with older (<=2.14.x) API and
        work with new 2.15.x API

--- burn-sharp/glue.c
+++ burn-sharp/glue.c
@@ -1,6 +1,10 @@
 
 #include <nautilus-burn-recorder.h>
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H */
+
 struct NautilusBurnRecorderTrack *
 nautilus_burn_glue_create_track (const char *filename,
 				 NautilusBurnRecorderTrackType type)
@@ -23,35 +27,59 @@ nautilus_burn_glue_create_track (const char *filename,
 char *
 nautilus_burn_glue_drive_get_id (NautilusBurnDrive *drive)
 {
+#ifndef HAVE_LIBNAUTILUS_BURN_4
 	return g_strdup (drive->cdrecord_id);
+#else /* HAVE_LIBNAUTILUS_BURN_4 */
+	return g_strdup (nautilus_burn_drive_get_device (drive));
+#endif /* HAVE_LIBNAUTILUS_BURN_4 */
 }
 
 NautilusBurnDriveType
 nautilus_burn_glue_drive_get_type (NautilusBurnDrive *drive)
 {
+#ifndef HAVE_LIBNAUTILUS_BURN_4
 	return drive->type;
+#else /* HAVE_LIBNAUTILUS_BURN_4 */
+	return nautilus_burn_drive_get_drive_type (drive);
+#endif /* HAVE_LIBNAUTILUS_BURN_4 */
 }
 
 char *
 nautilus_burn_glue_drive_get_display_name (NautilusBurnDrive *drive)
 {
+#ifndef HAVE_LIBNAUTILUS_BURN_4
 	return g_strdup (drive->display_name);
+#else /* HAVE_LIBNAUTILUS_BURN_4 */
+	return nautilus_burn_drive_get_name_for_display (drive);
+#endif /* HAVE_LIBNAUTILUS_BURN_4 */
 }
 
 int
 nautilus_burn_glue_drive_get_max_read_speed (NautilusBurnDrive *drive)
 {
+#ifndef HAVE_LIBNAUTILUS_BURN_4
 	return drive->max_speed_read;
+#else /* HAVE_LIBNAUTILUS_BURN_4 */
+	return nautilus_burn_drive_get_max_speed_read (drive);
+#endif /* HAVE_LIBNAUTILUS_BURN_4 */
 }
 
 int
 nautilus_burn_glue_drive_get_max_write_speed (NautilusBurnDrive *drive)
 {
+#ifndef HAVE_LIBNAUTILUS_BURN_4
 	return drive->max_speed_write;
+#else /* HAVE_LIBNAUTILUS_BURN_4 */
+	return nautilus_burn_drive_get_max_speed_write (drive);
+#endif /* HAVE_LIBNAUTILUS_BURN_4 */
 }
 
 char *
 nautilus_burn_glue_drive_get_device (NautilusBurnDrive *drive)
 {
+#ifndef HAVE_LIBNAUTILUS_BURN_4
 	return g_strdup (drive->device);
+#else /* HAVE_LIBNAUTILUS_BURN_4 */
+	return g_strdup (nautilus_burn_drive_get_device (drive));
+#endif /* HAVE_LIBNAUTILUS_BURN_4 */
 }
