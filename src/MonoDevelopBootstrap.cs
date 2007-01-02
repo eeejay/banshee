9129eaaa0d5c0e6a382ea0e8d1e8470e327542c5
Aaron Bockover <abock@src.gnome.org>
2007-01-02 05:23:44 +0000

    Added more MD projects
    svn path=/trunk/banshee/; revision=1927

---
+++ src/MonoDevelopBootstrap.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Diagnostics;
+
+public class MonoDevelopBootstrap
+{
+    public static void Main()
+    {
+        Process process = Process.Start("make", "run");
+        process.WaitForExit();
+    }
+}
