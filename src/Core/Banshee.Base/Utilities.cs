382cd03fdb26286f1e1e191176df9ab4dfd9f7e9
Aaron Bockover <abock@gnome.org>
2007-03-31 22:34:05 +0000

    Added a fallback to setproctitle if prctl fails; this should work for BSD
    2007-03-31  Aaron Bockover  <abock@gnome.org>
    
        * src/Core/Banshee.Base/Utilities.cs (SetProcessName): Added a fallback
        to setproctitle if prctl fails; this should work for BSD
    
    
    svn path=/trunk/banshee/; revision=2187

--- src/Core/Banshee.Base/Utilities.cs
+++ src/Core/Banshee.Base/Utilities.cs
@@ -134,15 +134,23 @@ namespace Banshee.Base
             return color;
         }
         
-        [DllImport("libc")]
+        [DllImport("libc")] // Linux
         private static extern int prctl(int option, byte [] arg2, IntPtr arg3, IntPtr arg4, IntPtr arg5);
         
+        [DllImport("libc")] // BSD
+        private static extern void setproctitle(byte [] fmt, byte [] str_arg);
+
         public static void SetProcessName(string name)
         {
-            if(prctl(15 /* PR_SET_NAME */, Encoding.ASCII.GetBytes(name + "\0"), 
-                IntPtr.Zero, IntPtr.Zero, IntPtr.Zero) != 0) {
-                throw new ApplicationException("Error setting process name: " + 
-                    Mono.Unix.Native.Stdlib.GetLastError());
+            try {
+                if(prctl(15 /* PR_SET_NAME */, Encoding.ASCII.GetBytes(name + "\0"), 
+                    IntPtr.Zero, IntPtr.Zero, IntPtr.Zero) != 0) {
+                    throw new ApplicationException("Error setting process name: " + 
+                        Mono.Unix.Native.Stdlib.GetLastError());
+                }
+            } catch(EntryPointNotFoundException) {
+                setproctitle(Encoding.ASCII.GetBytes("%s\0"), 
+                    Encoding.ASCII.GetBytes(name + "\0"));
             }
         }
     }
