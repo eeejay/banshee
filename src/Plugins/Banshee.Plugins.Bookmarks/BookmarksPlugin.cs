900d5acfc42f5a48ce4d13ab855a70c5d5144c23
Gabriel Burt <gabriel.burt@gmail.com>
2007-04-14 00:56:28 +0000

    Clean the design of the plugin with clear distinctions between the plugin,
    2007-04-13  Gabriel Burt  <gabriel.burt@gmail.com>
    
    	* src/Plugins/Banshee.Plugins.Bookmarks/BookmarksPlugin.cs: Clean the
    	design of the plugin with clear distinctions between the plugin, the UI,
    	and the model.  Handle bookmarked tracks being removed, and escape
    	underscores in track names when putting them in menus, and make the plugin
    	name translatable.
    
    svn path=/trunk/banshee/; revision=2211

--- src/Plugins/Banshee.Plugins.Bookmarks/BookmarksPlugin.cs
+++ src/Plugins/Banshee.Plugins.Bookmarks/BookmarksPlugin.cs
@@ -24,6 +24,47 @@ namespace Banshee.Plugins.Bookmarks
 {
     public class BookmarksPlugin : Banshee.Plugins.Plugin
     {
+        private BookmarkUI ui;
+        
+        protected override string ConfigurationName { 
+            get { return "bookmarks"; } 
+        }
+        
+        public override string DisplayName { 
+            get { return Catalog.GetString("Bookmarks"); }
+        }
+        
+        public override string Description {
+            get { return Catalog.GetString( "Bookmark your position in tracks."); }
+        }
+        
+        public override string [] Authors {
+            get { return new string [] { "Gabriel Burt" }; }
+        }
+ 
+        protected override void PluginInitialize()
+        {
+            Bookmark.Initialize();
+        }
+        
+        protected override void InterfaceInitialize()
+        {
+            ui = BookmarkUI.Instance;
+        }
+
+        protected override void PluginDispose()
+        {
+            if (ui != null)
+                ui.Dispose();
+        }
+
+        public static readonly SchemaEntry<bool> EnabledSchema = new SchemaEntry<bool>(
+            "plugins.bookmarks", "enabled", false, "Plugin enabled", "Bookmarks plugin enabled"
+        );
+    }
+
+    public class BookmarkUI
+    {
         private Menu parent_menu;
         private Menu bookmark_menu;
         private Menu remove_menu;
@@ -37,58 +78,32 @@ namespace Banshee.Plugins.Bookmarks
         private Dictionary<Bookmark, MenuItem> select_items = new Dictionary<Bookmark, MenuItem>();
         private Dictionary<Bookmark, MenuItem> remove_items = new Dictionary<Bookmark, MenuItem>();
         private Dictionary<MenuItem, Bookmark> bookmark_map = new Dictionary<MenuItem, Bookmark>();
-        
-        protected override string ConfigurationName { 
-            get { return "bookmarks"; } 
-        }
-        
-        public override string DisplayName { 
-            get { return "Bookmarks"; } 
-        }
-        
-        public override string Description {
-            get {
-                return Catalog.GetString(
-                    "Bookmark your position in tracks."
-                );
-            }
-        }
-        
-        public override string [] Authors {
-            get { 
-                return new string [] {
-                    "Gabriel Burt"
-                };
-            }
-        }
- 
-        // --------------------------------------------------------------- //
 
-        protected override void PluginInitialize()
-        {
-            if(!Globals.Library.Db.TableExists("Bookmarks")) {
-                Globals.Library.Db.Execute(@"
-                    CREATE TABLE Bookmarks (
-                        BookmarkID          INTEGER PRIMARY KEY,
-                        TrackID             INTEGER NOT NULL,
-                        Position            INTEGER NOT NULL,
-                        CreatedAt           INTEGER NOT NULL
-                    )
-                ");
+        private static BookmarkUI instance = null;
+        public static BookmarkUI Instance {
+            get {
+                if (instance == null)
+                    instance = new BookmarkUI();
+                return instance;
             }
         }
-        
-        protected override void InterfaceInitialize()
+
+        public static bool Instantiated {
+            get { return instance != null; }
+        }
+
+        private BookmarkUI()
         {
             parent_menu = (Globals.ActionManager.GetWidget("/MainMenu/ToolsMenu") as MenuItem).Submenu as Menu;
 
             bookmark_item = new ImageMenuItem(Catalog.GetString("_Bookmarks"));
             bookmark_item.Image = new Image ("bookmark", IconSize.Menu);
+            bookmark_item.AccelPath = "<control>D";
             bookmark_item.Submenu = bookmark_menu = new Menu();
 
             new_item = new ImageMenuItem(Catalog.GetString("_New Bookmark"));
             new_item.Image = new Image(Stock.Add, IconSize.Menu);
-            new_item.Activated += OnNewBookmark;
+            new_item.Activated += HandleNewBookmark;
             bookmark_item.Selected += HandleMenuShown;
 
             bookmark_menu.Append(new_item);
@@ -114,17 +129,12 @@ namespace Banshee.Plugins.Bookmarks
             new_item.Sensitive = (PlayerEngineCore.CurrentTrack != null);
         }
 
-        private void OnNewBookmark(object sender, EventArgs args)
+        private void HandleNewBookmark(object sender, EventArgs args)
         {
             TrackInfo track = PlayerEngineCore.CurrentTrack;
             if (track != null) {
-                AddBookmark(new Bookmark(track, PlayerEngineCore.Position));
+                AddBookmark(new Bookmark(track.TrackId, PlayerEngineCore.Position));
             }
-        }
-
-        private void OnRemoveBookmark(object sender, EventArgs args)
-        {
-            RemoveBookmark(bookmark_map[sender as MenuItem]);
         }
 
         private void HandleLibraryReloaded (object sender, EventArgs args)
@@ -140,15 +150,19 @@ namespace Banshee.Plugins.Bookmarks
             bookmark_item.ShowAll();
         }
         
-        private void AddBookmark(Bookmark bookmark)
+        public void AddBookmark(Bookmark bookmark)
         {
+            if (select_items.ContainsKey(bookmark))
+                return;
+
             bookmarks.Add(bookmark);
             if (bookmarks.Count == 1) {
                 bookmark_menu.Append(separator);
                 remove_item.Sensitive = true;
             }
 
-            ImageMenuItem select_item = new ImageMenuItem(bookmark.Name);
+            // Add menu item to jump to this bookmark
+            ImageMenuItem select_item = new ImageMenuItem(bookmark.Name.Replace("_", "__"));
             select_item.Image = new Image(Stock.JumpTo, IconSize.Menu);
             select_item.Activated += delegate {
                 bookmark.JumpTo();
@@ -156,10 +170,11 @@ namespace Banshee.Plugins.Bookmarks
             bookmark_menu.Append(select_item);
             select_items[bookmark] = select_item;
 
-            ImageMenuItem rem = new ImageMenuItem(bookmark.Name);
+            // Add menu item to remove this bookmark
+            ImageMenuItem rem = new ImageMenuItem(bookmark.Name.Replace("_", "__"));
             rem.Image = new Image(Stock.Remove, IconSize.Menu);
             rem.Activated += delegate {
-                RemoveBookmark(bookmark);
+                bookmark.Remove();
             };
             remove_menu.Append(rem);
             remove_items[bookmark] = rem;
@@ -168,12 +183,14 @@ namespace Banshee.Plugins.Bookmarks
             bookmark_menu.ShowAll();
         }
 
-        private void RemoveBookmark(Bookmark bookmark)
+        public void RemoveBookmark(Bookmark bookmark)
         {
+            if (!remove_items.ContainsKey(bookmark))
+                return;
+
             bookmark_menu.Remove(select_items[bookmark]);
             remove_menu.Remove(remove_items[bookmark]);
             bookmarks.Remove(bookmark);
-            bookmark.Remove();
             select_items.Remove(bookmark);
             bookmark_map.Remove(remove_items[bookmark]);
             remove_items.Remove(bookmark);
@@ -181,70 +198,64 @@ namespace Banshee.Plugins.Bookmarks
             if (bookmarks.Count == 0) {
                 bookmark_menu.Remove(separator);
                 remove_item.Sensitive = false;
-            }
+           }
         }
 
-        protected override void PluginDispose()
+        public void Dispose()
         {
             if (parent_menu != null && bookmark_item != null) {
                 parent_menu.Remove(bookmark_item);
             }
+            instance = null;
         }
-
-        public static readonly SchemaEntry<bool> EnabledSchema = new SchemaEntry<bool>(
-            "plugins.bookmarks", "enabled",
-            false,
-            "Plugin enabled",
-            "Bookmarks plugin enabled"
-        );
     }
 
     public class Bookmark
     {
         private int id;
-        private TrackInfo track;
+        private int track_id;
         private uint position;
         private DateTime created_at;
-
-        private MenuItem select_menu_item;
-        private MenuItem remove_menu_item;
 
         // Translators: This is used to generate bookmark names. {0} is track title, {1} is minutes
         // (possibly more than two digits) and {2} is seconds (between 00 and 60).
         private readonly string bookmark_format = Catalog.GetString("{0} ({1}:{2:00})");
 
+        private string name;
         public string Name {
-            get { return String.Format(bookmark_format, track.DisplayTitle, position/60, position%60); }
+            get { return name; }
+            set { name = value; }
         }
 
         public DateTime CreatedAt {
             get { return created_at; }
         }
 
-        private Bookmark(int id, TrackInfo track, uint position, DateTime created_at)
-        {
-            this.id = id;
-            this.position = position;
-            this.track = track;
-            this.created_at = created_at;
-
-            //this.track.Removed += HandleTrackRemoved;
+        public TrackInfo Track {
+            get { return Globals.Library.Tracks.ContainsKey(track_id) ? Globals.Library.GetTrack(track_id) : null; }
         }
 
-        public Bookmark(TrackInfo track, uint position)
+        private Bookmark(int id, int track_id, uint position, DateTime created_at)
         {
-            this.track = track;
+            this.id = id;
+            this.track_id = track_id;
+            this.position = position;
+            this.created_at = created_at;
+            Name = String.Format(bookmark_format, Track.DisplayTitle, position/60, position%60);
+        }
+
+        public Bookmark(int track_id, uint position)
+        {
+            this.track_id = track_id;
             this.position = position;
             this.created_at = DateTime.Now;
-
-            select_menu_item = new MenuItem(Name);
-            remove_menu_item = new MenuItem(Name);
+            Name = String.Format(bookmark_format, Track.DisplayTitle, position/60, position%60);
 
             DbCommand command = new DbCommand(@"
                 INSERT INTO Bookmarks
                     (TrackID, Position, CreatedAt)
                     VALUES (:track_id, :position, :created_at)",
-                "track_id", track.TrackId, 
+                "track_id", track_id,
                 "position", position, 
                 "created_at", DateTimeUtil.FromDateTime(created_at)
             );
@@ -254,20 +265,30 @@ namespace Banshee.Plugins.Bookmarks
 
         public void JumpTo()
         {
-            if (PlayerEngineCore.CurrentTrack != track) {
-                PlayerEngineCore.OpenPlay(track); 
-            }
+            TrackInfo track = Track;
+            if (track != null) {
+                if (PlayerEngineCore.CurrentTrack != track) {
+                    PlayerEngineCore.OpenPlay(track); 
+                }
 
-            if (PlayerEngineCore.CanSeek) {
-                PlayerEngineCore.Position = position;
+                if (PlayerEngineCore.CanSeek) {
+                    PlayerEngineCore.Position = position;
+                }
+            } else {
+                Remove();
             }
         }
 
         public void Remove()
         {
-            Globals.Library.Db.Execute(String.Format(
-                "DELETE FROM Bookmarks WHERE BookmarkID = {0}", id
-            ));
+            try {
+                Globals.Library.Db.Execute(String.Format(
+                    "DELETE FROM Bookmarks WHERE BookmarkID = {0}", id
+                ));
+
+                if (BookmarkUI.Instantiated)
+                    BookmarkUI.Instance.RemoveBookmark(this);
+            } catch {}
         }
 
         public static List<Bookmark> LoadAll()
@@ -279,17 +300,34 @@ namespace Banshee.Plugins.Bookmarks
             );
 
             while (reader.Read()) {
-                TrackInfo track = Globals.Library.GetTrack((int) reader[1]);
-                if (track != null) {
+                try {
                     bookmarks.Add(new Bookmark(
-                        (int)reader[0], track, (uint)(int)reader[2],
+                        (int) reader[0], (int) reader[1], (uint)(int) reader[2],
                         DateTimeUtil.ToDateTime(Convert.ToInt64(reader[3]))
+                    ));
+                } catch {
+                    Globals.Library.Db.Execute(String.Format(
+                        "DELETE FROM Bookmarks WHERE BookmarkID = {0}", (int)reader[1]
                     ));
                 }
             }
             reader.Dispose();
 
             return bookmarks;
+        }
+
+        public static void Initialize()
+        {
+            if (!Globals.Library.Db.TableExists("Bookmarks")) {
+                Globals.Library.Db.Execute(@"
+                    CREATE TABLE Bookmarks (
+                        BookmarkID          INTEGER PRIMARY KEY,
+                        TrackID             INTEGER NOT NULL,
+                        Position            INTEGER NOT NULL,
+                        CreatedAt           INTEGER NOT NULL
+                    )
+                ");
+            }
         }
     }
 }
