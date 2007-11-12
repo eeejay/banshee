1a29d119d0f62537713e98e9da3782720a8a8173
Aaron Bockover <abock@gnome.org>
2007-11-12 23:30:12 +0000

    Use my new autogen script that's been used in podsleuth and other projects
    2007-11-12  Aaron Bockover  <abock@gnome.org>
    
        * autogen.sh: Use my new autogen script that's been used in podsleuth
        and other projects - no dependency on gnome-autogen, which has been a
        big pain for banshee; also sets -Wno-portability -Wno-syntax on automake
        so it will STFU about our Makefile.ams, which has been a problem in
        Mono projects and the latest automake
    
    
    svn path=/trunk/banshee/; revision=2766

--- autogen.sh
+++ autogen.sh
@@ -1,36 +1,74 @@
 #!/bin/bash
-# Run this to generate all the initial makefiles, etc.
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
+PROJECT=banshee
 
-PKG_NAME="banshee"
-
-(test -f $srcdir/configure.ac) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level $PKG_NAME directory"
-    exit 1
-}
-
-which svn 1>/dev/null || {
-	echo "**Error**: subversion was not found, please install it"
+function error () {
+	echo "Error: $1" 1>&2
 	exit 1
 }
 
-which gnome-autogen.sh || {
-    echo "You need to install gnome-common from the GNOME CVS"
-    exit 1
+function check_autotool_version () {
+	which $1 &>/dev/null || {
+		error "$1 is not installed, and is required to configure $PACKAGE"
+	}
+
+	version=$($1 --version | head -n 1 | cut -f4 -d' ')
+	major=$(echo $version | cut -f1 -d.)
+	minor=$(echo $version | cut -f2 -d.)
+	rev=$(echo $version | cut -f3 -d.)
+	major_check=$(echo $2 | cut -f1 -d.)
+	minor_check=$(echo $2 | cut -f2 -d.)
+	rev_check=$(echo $2 | cut -f3 -d.)
+
+	if [ $major -lt $major_check ]; then
+		do_bail=yes
+	elif [[ $minor -lt $minor_check && $major = $major_check ]]; then
+		do_bail=yes
+	elif [[ $rev -lt $rev_check && $minor = $minor_check && $major = $major_check ]]; then
+		do_bail=yes
+	fi
+
+	if [ x"$do_bail" = x"yes" ]; then
+		error "$1 version $2 or better is required to configure $PROJECT"
+	fi
 }
 
-ACLOCAL_FLAGS="-I build/m4/shamrock -I build/m4/banshee $ACLOCAL_FLAGS" REQUIRED_AUTOMAKE_VERSION=1.9 USE_GNOME2_MACROS=1 . gnome-autogen.sh
+function run () {
+	echo "Running $@ ..."
+	$@ 2>.autogen.log || {
+		cat .autogen.log 1>&2
+		rm .autogen.log
+		error "Could not run $1, which is required to configure $PROJECT"
+	}
+	rm .autogen.log
+}
 
-if ! test -x ./mkinstalldirs; then 
-	for automake_path in `whereis automake-1.9`; do 
-		if ! test -z `echo $automake_path | grep share`; then 
-			if test -x $automake_path/mkinstalldirs; then
-				cp $automake_path/mkinstalldirs .
-			fi
-		fi 
-	done
+srcdir=$(dirname $0)
+test -z "$srcdir" && srcdir=.
+
+(test -f $srcdir/configure.ac) || {
+	error "Directory \"$srcdir\" does not look like the top-level $PROJECT directory"
+}
+
+check_autotool_version aclocal 1.9
+check_autotool_version automake 1.9
+check_autotool_version autoconf 2.53
+check_autotool_version libtoolize 1.4.3
+check_autotool_version intltoolize 0.35.0
+check_autotool_version pkg-config 0.14.0
+
+run libtoolize --force --copy
+run intltoolize --force --copy --automake
+run aclocal -I build/m4/banshee -I build/m4/shamrock
+run autoconf
+run autoheader
+test -f config.h.in && touch config.h.in
+run automake --gnu --add-missing --force --copy \
+	-Wno-portability -Wno-portability
+
+if [ $# = 0 ]; then
+	echo "WARNING: I am going to run configure without any arguments."
 fi
+
+run ./configure --enable-maintainer-mode $@
 
