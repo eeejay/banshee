f03f30d1dafdf4bf3dcaa3a7f12ecdd1cc933904
Aaron Bockover <abock@src.gnome.org>
2008-01-27 06:23:50 +0000

    Major build changes
    svn path=/trunk/banshee/; revision=3054

--- build/build.rules.mk
+++ build/build.rules.mk
@@ -8,22 +8,25 @@ RESOURCES_BUILD = $(foreach resource, $(RESOURCES_EXPANDED_FULL), \
 	-resource:$(resource),$(notdir $(resource)))
 
 ASSEMBLY_EXTENSION = $(strip $(patsubst library, dll, $(TARGET)))
-ASSEMBLY_FILE = $(ASSEMBLY).$(ASSEMBLY_EXTENSION)
+ASSEMBLY_FILE = $(top_builddir)/bin/$(ASSEMBLY).$(ASSEMBLY_EXTENSION)
 
 INSTALL_DIR_RESOLVED = $(firstword $(subst , $(DEFAULT_INSTALL_DIR), $(INSTALL_DIR)))
 
 moduledir = $(INSTALL_DIR_RESOLVED)
-module_SCRIPTS = $(ASSEMBLY_FILE) $(ASSEMBLY_FILE).mdb
+module_SCRIPTS = 
+
+OUTPUT_FILES = \
+	$(ASSEMBLY_FILE) \
+	$(ASSEMBLY_FILE).mdb
 
 all: $(ASSEMBLY_FILE)
 
 $(ASSEMBLY_FILE): $(SOURCES_BUILD) $(RESOURCES_EXPANDED_FULL)
-	@echo "$(SOURCES_BUILD)" | tr [:space:] \\n > $(ASSEMBLY_FILE).sources
-	@mkdir -p $(top_builddir)/build
-	$(BUILD) -target:$(TARGET) -out:$@ $(LINK) $(RESOURCES_BUILD) @$(ASSEMBLY_FILE).sources
-	@rm -f $(ASSEMBLY_FILE).sources
+	@mkdir -p $(top_builddir)/bin
+	@echo "Compiling $(notdir $@)..."
+	@$(BUILD) -target:$(TARGET) -out:$@ $(LINK) $(RESOURCES_BUILD) $(SOURCES_BUILD)
 
 EXTRA_DIST = $(SOURCES_BUILD) $(RESOURCES_EXPANDED_FULL)
 
-CLEANFILES = $(ASSEMBLY_FILE) $(ASSEMBLY_FILE).mdb *.dll *.mdb *.exe
+CLEANFILES = $(OUTPUT_FILES) *.dll *.mdb *.exe
 MAINTAINERCLEANFILES = Makefile.in
