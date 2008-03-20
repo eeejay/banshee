eeejay/banshee

commit 79eb97923d8b3619f7c4cbe2f963a81815461237

Take the title from the filename if needed.
2008-03-19  Gabriel Burt  <gabriel.burt@gmail.com>

	* src/Core/Banshee.Core/Banshee.Streaming/StreamTagger.cs: Take the title
	from the filename if needed.

	* src/Core/Banshee.ThickClient/Banshee.Gui.Widgets/TrackInfoDisplay.cs:
	Display a video icon for missing video 'album art'.

	* src/Extensions/Banshee.NotificationArea/Banshee.NotificationArea/NotificationAreaService.cs:
	Change 'song' to 'item' in a few places.

svn path=/trunk/banshee/; revision=3487

src/Core/Banshee.ThickClient/Banshee.Gui.Widgets/TrackInfoDisplay.cs
@@ -51,7 +51,8 @@ namespace Banshee.Gui.Widgets
         private ArtworkManager artwork_manager;
         private Gdk.Pixbuf current_pixbuf;
         private Gdk.Pixbuf incoming_pixbuf;
-        private Gdk.Pixbuf missing_pixbuf;
+        private Gdk.Pixbuf missing_audio_pixbuf;
+        private Gdk.Pixbuf missing_video_pixbuf;
         
         private Cairo.Color background_color;
         private Cairo.Color text_color;
@@ -239,9 +240,14 @@ namespace Banshee.Gui.Widgets
             text_light_color = CairoExtensions.ColorAdjustBrightness (text_color, 0.5);
             background_color = CairoExtensions.GdkColorToCairoColor (Style.Background (StateType.Normal));
             
-            if (missing_pixbuf != null) {
-                missing_pixbuf.Dispose ();
-                missing_pixbuf = null;
+            if (missing_audio_pixbuf != null) {
+                missing_audio_pixbuf.Dispose ();
+                missing_audio_pixbuf = null;
+            }
+
+            if (missing_video_pixbuf != null) {
+                missing_video_pixbuf.Dispose ();
+                missing_video_pixbuf = null;
             }
         }
         
@@ -344,7 +350,12 @@ namespace Banshee.Gui.Widgets
         private void RenderCoverArt (Cairo.Context cr, Gdk.Pixbuf pixbuf)
         {
             ArtworkRenderer.RenderThumbnail (cr, pixbuf, false, 0, 0, Allocation.Height, Allocation.Height, 
-                pixbuf != missing_pixbuf, 0, pixbuf == missing_pixbuf, background_color);
+                IsMissingPixbuf (pixbuf), 0, IsMissingPixbuf (pixbuf), background_color);
+        }
+
+        private bool IsMissingPixbuf (Gdk.Pixbuf pb)
+        {
+            return (pb == missing_audio_pixbuf || pb == missing_video_pixbuf);
         }
         
         private void RenderTrackInfo (Cairo.Context cr, TrackInfo track, bool renderTrack, bool renderArtistAlbum)
@@ -411,7 +422,7 @@ namespace Banshee.Gui.Widgets
         {
             TrackInfo track = ServiceManager.PlayerEngine.CurrentTrack;
 
-            if (track == current_track && current_pixbuf != missing_pixbuf) {
+            if (track == current_track && !IsMissingPixbuf (current_pixbuf)) {
                 return;
             } else if (track == null) {
                 incoming_track = null;
@@ -424,10 +435,17 @@ namespace Banshee.Gui.Widgets
             Gdk.Pixbuf pixbuf = artwork_manager.LookupScale (track.ArtistAlbumId, Allocation.Height);
             
             if (pixbuf == null) {
-                if (missing_pixbuf == null) {
-                    missing_pixbuf = IconThemeUtils.LoadIcon (32, "audio-x-generic");
+                if ((track.MediaAttributes & TrackMediaAttributes.VideoStream) != 0) {
+                    if (missing_video_pixbuf == null) {
+                        missing_video_pixbuf = IconThemeUtils.LoadIcon (32, "video-x-generic");
+                    }
+                    incoming_pixbuf = missing_video_pixbuf;
+                } else {
+                    if (missing_audio_pixbuf == null) {
+                        missing_audio_pixbuf = IconThemeUtils.LoadIcon (32, "audio-x-generic");
+                    }
+                    incoming_pixbuf = missing_audio_pixbuf;
                 }
-                incoming_pixbuf = missing_pixbuf;
             } else {
                 incoming_pixbuf = pixbuf;
             }
@@ -452,7 +470,7 @@ namespace Banshee.Gui.Widgets
                 Log.DebugFormat ("TrackInfoDisplay RenderAnimation: {0:0.00} FPS", last_fps);
             }
             
-            if (current_pixbuf != incoming_pixbuf && current_pixbuf != missing_pixbuf) {
+            if (current_pixbuf != incoming_pixbuf && !IsMissingPixbuf (current_pixbuf)) {
                 ArtworkRenderer.DisposePixbuf (current_pixbuf);
             }
             

src/Core/Banshee.Core/Banshee.Streaming/StreamTagger.cs
@@ -124,6 +124,16 @@ namespace Banshee.Streaming
             track.TrackCount = Choose ((int)file.Tag.TrackCount, track.TrackCount, preferTrackInfo);
             track.Disc = Choose ((int)file.Tag.Disc, track.Disc, preferTrackInfo);
             track.Year = Choose ((int)file.Tag.Year, track.Year, preferTrackInfo);
+
+            if (String.IsNullOrEmpty (track.TrackTitle)) {
+                try {
+                    string filename = System.IO.Path.GetFileName (track.Uri.LocalPath);
+                    if (!String.IsNullOrEmpty (filename)) {
+                        filename = filename.Substring (0, filename.LastIndexOf ('.'));
+                        track.TrackTitle = filename;
+                    }
+                } catch {}
+            }
         }
     
         public static void TrackInfoMerge (TrackInfo track, StreamTag tag)

src/Extensions/Banshee.NotificationArea/Banshee.NotificationArea/NotificationAreaService.cs
@@ -120,7 +120,7 @@ namespace Banshee.NotificationArea
             actions.Add (new ToggleActionEntry [] {
                 new ToggleActionEntry ("ToggleNotificationsAction", null,
                     Catalog.GetString ("_Show Notifications"), null,
-                    Catalog.GetString ("Show notifications when song changes"), ToggleNotifications, ShowNotifications)
+                    Catalog.GetString ("Show notifications when item changes"), ToggleNotifications, ShowNotifications)
             });
             
             interface_action_service.AddActionGroup (actions);
@@ -364,7 +364,7 @@ namespace Banshee.NotificationArea
                 nf.Urgency = Urgency.Low;
                 nf.Timeout = 4500;
                 if (interface_action_service.PlaybackActions["NextAction"].Sensitive) {
-                    nf.AddAction ("skip-song", Catalog.GetString("Play next song"), OnSongSkipped);
+                    nf.AddAction ("skip-song", Catalog.GetString("Play next item"), OnSongSkipped);
                 }
                 nf.Show ();
                 
@@ -423,7 +423,7 @@ namespace Banshee.NotificationArea
             "plugins.notification_area", "show_notifications",
             true,
             "Show notifications",
-            "Show track information notifications when track starts playing"
+            "Show information notifications when item starts playing"
         );
                 
         public static readonly SchemaEntry<bool> NotifyOnCloseSchema = new SchemaEntry<bool> (

commit f69adaf508d269fc47e6fb2447e5781ad562c5b6

Added left padding, so it doesn't look so bad on fullscreen. Also made
2008-03-20  Alexander Hixon  <hixon.alexander@mediati.org>

	* src/Core/Banshee.ThickClient/Banshee.Gui.Widgets/UserJobTileHost.cs:
	Added left padding, so it doesn't look so bad on fullscreen. Also made
	addition to code comment from previous commit, and some evil whitespace.

	* src/Core/Banshee.ThickClient/Banshee.Gui.Widgets/TrackInfoDisplay.cs:
	Patch to fix small bug introduced in r3487 where the missing pixbuf icon
	would be displayed as an album cover type when it shouldn't be (ie the
	black box around the icon).

	* src/Core/Banshee.Services/Banshee.Sources/Source.cs: Slightly more
	awesome status bar text, shamelessly ported from stable.

	* src/Core/Banshee.Services/Banshee.Collection/ImportManager.cs: Removed
	more mentions of 'songs' and replaced with 'media'.

svn path=/trunk/banshee/; revision=3492

src/Core/Banshee.Services/Banshee.Collection/ImportManager.cs
@@ -73,7 +73,7 @@ namespace Banshee.Collection
                     return;
                 }
                 
-                user_job = new UserJob (Title, Title, Catalog.GetString ("Scanning for songs"));
+                user_job = new UserJob (Title, Title, Catalog.GetString ("Scanning for media"));
                 user_job.IconNames = new string [] { "system-search", "gtk-find" };
                 user_job.CancelMessage = CancelMessage;
                 user_job.CanCancel = true;
@@ -274,7 +274,7 @@ namespace Banshee.Collection
             }
         }
         
-        private string title = Catalog.GetString ("Importing Songs");
+        private string title = Catalog.GetString ("Importing Media");
         public string Title {
             get { return title; }
             set { title = value; }

src/Core/Banshee.Services/Banshee.Sources/Source.cs
@@ -430,22 +430,28 @@ namespace Banshee.Sources
             builder.AppendFormat (Catalog.GetPluralString ("{0} item", "{0} items", count), count);
             
             if (this is IDurationAggregator) {
-                builder.Append (", ");
-
-                TimeSpan span = (this as IDurationAggregator).Duration; 
+                TimeSpan span = (this as IDurationAggregator).Duration;
+                
                 if (span.Days > 0) {
-                    double days = span.Days + (span.Hours / 24.0);
-                    builder.AppendFormat (Catalog.GetPluralString ("{0} day", "{0:0.0} days", 
-                        (int)Math.Ceiling (days)), days);
-                } else if (span.Hours > 0) {
-                    double hours = span.Hours + (span.Minutes / 60.0);
-                    builder.AppendFormat (Catalog.GetPluralString ("{0} hour", "{0:0.0} hours", 
-                        (int)Math.Ceiling (hours)), hours);
-                } else {
-                    double minutes = span.Minutes + (span.Seconds / 60.0);
-                    builder.AppendFormat (Catalog.GetPluralString ("{0} minute", "{0:0.0} minutes", 
-                        (int)Math.Ceiling (minutes)), minutes);
+                    builder.Append (", ");
+                    builder.AppendFormat (Catalog.GetPluralString ("{0} day", "{0} days", 
+                        (int) span.Days), span.Days);
                 }
+                
+                if (span.Hours > 0) {
+                    builder.Append (", ");
+                    builder.AppendFormat (Catalog.GetPluralString ("{0} hour", "{0} hours", 
+                        (int) span.Hours), span.Hours);
+                }
+                
+                builder.Append (", ");
+                
+                builder.AppendFormat (Catalog.GetPluralString ("{0} minute", "{0} minutes", 
+                    (int) span.Minutes), span.Minutes);
+                
+                builder.Append (", ");
+                builder.AppendFormat (Catalog.GetPluralString ("{0} second", "{0} seconds",
+                    (int) span.Seconds), span.Seconds);
             }
 
             if (this is IFileSizeAggregator) {

src/Core/Banshee.ThickClient/Banshee.Gui.Widgets/TrackInfoDisplay.cs
@@ -350,7 +350,7 @@ namespace Banshee.Gui.Widgets
         private void RenderCoverArt (Cairo.Context cr, Gdk.Pixbuf pixbuf)
         {
             ArtworkRenderer.RenderThumbnail (cr, pixbuf, false, 0, 0, Allocation.Height, Allocation.Height, 
-                IsMissingPixbuf (pixbuf), 0, IsMissingPixbuf (pixbuf), background_color);
+                !IsMissingPixbuf (pixbuf), 0, IsMissingPixbuf (pixbuf), background_color);
         }
 
         private bool IsMissingPixbuf (Gdk.Pixbuf pb)

src/Core/Banshee.ThickClient/Banshee.Gui.Widgets/UserJobTileHost.cs
@@ -44,6 +44,8 @@ namespace Banshee.Gui.Widgets
         
         public UserJobTileHost () : base (0.0f, 0.0f, 1.0f, 1.0f)
         {
+            LeftPadding = 4;
+            
             box = new AnimatedVBox ();
             box.Spacing = 8;
             box.Show ();
@@ -114,7 +116,7 @@ namespace Banshee.Gui.Widgets
             ThreadAssist.ProxyToMain (delegate {
                 lock (this) {
                     if (job_tiles.ContainsKey (args.Job)) {
-                        UserJobTile tile = job_tiles[args.Job];                        
+                        UserJobTile tile = job_tiles[args.Job];
                         box.Remove (tile);
                         job_tiles.Remove (args.Job);
                     }
@@ -122,6 +124,9 @@ namespace Banshee.Gui.Widgets
                     if (job_tiles.Count <= 0) {
                         // Don't actually hide the widget, since that screws
                         // with the last widget's ease-out animation.
+                        
+                        // All subwidgets end up hiding themselves anyway,
+                        // so all we need to do is make sure we have no padding.
                         TopPadding = 0;
                     }
                 }