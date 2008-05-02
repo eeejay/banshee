1c33e6d337c5760cd439752ddb71f88d17e70f19
Gabriel Burt <gabriel.burt@gmail.com>
2008-05-02 19:39:37 +0000

    Update to not use ApplyValues; fix the build.
    2008-05-02  Gabriel Burt  <gabriel.burt@gmail.com>
    
    	* tests/Hyena/SqliteCommandTests.cs: Update to not use ApplyValues; fix
    	the build.
    
    svn path=/trunk/banshee/; revision=3864

--- tests/Hyena/SqliteCommandTests.cs
+++ tests/Hyena/SqliteCommandTests.cs
@@ -9,15 +9,16 @@ public class SqliteCommandTests
     [Test]
     public void TestIdentifiesParameters ()
     {
-        HyenaSqliteCommand cmd = new HyenaSqliteCommand ("select foo from bar where baz = ?, bbz = ?, this = ?");
-
+        HyenaSqliteCommand cmd = null;
         try {
-            cmd.ApplyValues ("a", 32);
+            cmd = new HyenaSqliteCommand ("select foo from bar where baz = ?, bbz = ?, this = ?",
+                "a", 32);
             Assert.Fail ("Should not have been able to pass 2 values to ApplyValues without exception");
         } catch {}
 
         try {
-            cmd.ApplyValues ("a", 32, "22");
+            cmd = new HyenaSqliteCommand ("select foo from bar where baz = ?, bbz = ?, this = ?",
+                "a", 32, "22");
         } catch {
             Assert.Fail ("Should have been able to pass 3 values to ApplyValues without exception");
         }
@@ -44,25 +45,30 @@ public class SqliteCommandTests
     {
         HyenaSqliteCommand cmd = new HyenaSqliteCommand ("select foo from bar where baz = ?");
 
-        Assert.AreEqual ("select foo from bar where baz = NULL", GetGeneratedSql (cmd.ApplyValues (null)));
-        Assert.AreEqual ("select foo from bar where baz = 'It''s complicated, \"but\" ''''why not''''?'", GetGeneratedSql (cmd.ApplyValues ("It's complicated, \"but\" ''why not''?")));
-        Assert.AreEqual ("select foo from bar where baz = 0", GetGeneratedSql (cmd.ApplyValues (new DateTime (1970, 1, 1).ToLocalTime ())));
-        Assert.AreEqual ("select foo from bar where baz = 931309200", GetGeneratedSql (cmd.ApplyValues (new DateTime (1999, 7, 7).ToLocalTime ())));
-        Assert.AreEqual ("select foo from bar where baz = 555.55", GetGeneratedSql (cmd.ApplyValues (555.55f)));
-        Assert.AreEqual ("select foo from bar where baz = 555.55", GetGeneratedSql (cmd.ApplyValues (555.55)));
-        Assert.AreEqual ("select foo from bar where baz = 555", GetGeneratedSql (cmd.ApplyValues (555)));
-        Assert.AreEqual ("select foo from bar where baz = 1", GetGeneratedSql (cmd.ApplyValues (true)));
-        Assert.AreEqual ("select foo from bar where baz = 0", GetGeneratedSql (cmd.ApplyValues (false)));
+        Assert.AreEqual ("select foo from bar where baz = NULL", GetGeneratedSql (cmd, null));
+        Assert.AreEqual ("select foo from bar where baz = 'It''s complicated, \"but\" ''''why not''''?'", GetGeneratedSql (cmd, "It's complicated, \"but\" ''why not''?"));
+        Assert.AreEqual ("select foo from bar where baz = 0", GetGeneratedSql (cmd, new DateTime (1970, 1, 1).ToLocalTime ()));
+        Assert.AreEqual ("select foo from bar where baz = 931309200", GetGeneratedSql (cmd, new DateTime (1999, 7, 7).ToLocalTime ()));
+        Assert.AreEqual ("select foo from bar where baz = 555.55", GetGeneratedSql (cmd, 555.55f));
+        Assert.AreEqual ("select foo from bar where baz = 555.55", GetGeneratedSql (cmd, 555.55));
+        Assert.AreEqual ("select foo from bar where baz = 555", GetGeneratedSql (cmd, 555));
+        Assert.AreEqual ("select foo from bar where baz = 1", GetGeneratedSql (cmd, true));
+        Assert.AreEqual ("select foo from bar where baz = 0", GetGeneratedSql (cmd, false));
 
         HyenaSqliteCommand cmd2 = new HyenaSqliteCommand ("select foo from bar where baz = ?, bar = ?, boo = ?");
-        Assert.AreEqual ("select foo from bar where baz = NULL, bar = NULL, boo = 22", GetGeneratedSql (cmd2.ApplyValues (null, null, 22)));
+        Assert.AreEqual ("select foo from bar where baz = NULL, bar = NULL, boo = 22", GetGeneratedSql (cmd2, null, null, 22));
 
         HyenaSqliteCommand cmd3 = new HyenaSqliteCommand ("select foo from bar where id in (?) and foo not in (?)");
         Assert.AreEqual ("select foo from bar where id in (1,2,4) and foo not in ('foo','baz')",
-                GetGeneratedSql (cmd3.ApplyValues (new int [] {1, 2, 4}, new string [] {"foo", "baz"})));
+                GetGeneratedSql (cmd3, new int [] {1, 2, 4}, new string [] {"foo", "baz"}));
     }
 
     static PropertyInfo tf = typeof(HyenaSqliteCommand).GetProperty ("CurrentSqlText", BindingFlags.Instance | BindingFlags.NonPublic);
+    private static string GetGeneratedSql (HyenaSqliteCommand cmd, params object [] p)
+    {
+        return tf.GetValue ((new HyenaSqliteCommand (cmd.Text, p)), null) as string;
+    }
+
     private static string GetGeneratedSql (HyenaSqliteCommand cmd)
     {
         return tf.GetValue (cmd, null) as string;
