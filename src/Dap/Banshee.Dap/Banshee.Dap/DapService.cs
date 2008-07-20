f1251a52c31d409f9d22be6718603eb90bc8fa6b
Gabriel Burt <gabriel.burt@gmail.com>
2008-07-20 18:12:29 +0000

    Use Banshee.Kernel instead of ThreadAssist.SpawnFromMain.
    2008-07-20  Gabriel Burt  <gabriel.burt@gmail.com>
    
    	* src/Dap/Banshee.Dap/Banshee.Dap/DapService.cs: Use Banshee.Kernel
    	instead of ThreadAssist.SpawnFromMain.
    
    svn path=/trunk/banshee/; revision=4222

--- src/Dap/Banshee.Dap/Banshee.Dap/DapService.cs
+++ src/Dap/Banshee.Dap/Banshee.Dap/DapService.cs
@@ -141,33 +141,37 @@ namespace Banshee.Dap
         
         private void MapDevice (IDevice device)
         {
-            Banshee.Base.ThreadAssist.SpawnFromMain (delegate {
+            Banshee.Kernel.Scheduler.Schedule (new Banshee.Kernel.DelegateJob (delegate {
                 lock (this) {
-                    if (sources.ContainsKey (device.Uuid)) {
-                        return;
-                    }
-                    
-                    if (device is ICdromDevice || device is IDiscVolume) {
-                        return;
-                    }
-                    
-                    if (device is IVolume && (device as IVolume).ShouldIgnore) {
-                        return;
-                    }
-                    
-                    if (device.MediaCapabilities == null && !(device is IBlockDevice) && !(device is IVolume)) {
-                        return;
-                    }
-                    
-                    DapSource source = FindDeviceSource (device);
-                    if (source != null) {
-                        Log.DebugFormat ("Found DAP support ({0}) for device {1}", source.GetType ().FullName, source.Name);
-                        sources.Add (device.Uuid, source);
-                        ServiceManager.SourceManager.AddSource (source);
-                        source.NotifyUser ();
+                    try {
+                        if (sources.ContainsKey (device.Uuid)) {
+                            return;
+                        }
+                        
+                        if (device is ICdromDevice || device is IDiscVolume) {
+                            return;
+                        }
+                        
+                        if (device is IVolume && (device as IVolume).ShouldIgnore) {
+                            return;
+                        }
+                        
+                        if (device.MediaCapabilities == null && !(device is IBlockDevice) && !(device is IVolume)) {
+                            return;
+                        }
+                        
+                        DapSource source = FindDeviceSource (device);
+                        if (source != null) {
+                            Log.DebugFormat ("Found DAP support ({0}) for device {1}", source.GetType ().FullName, source.Name);
+                            sources.Add (device.Uuid, source);
+                            ServiceManager.SourceManager.AddSource (source);
+                            source.NotifyUser ();
+                        }
+                    } catch (Exception e) {
+                        Log.Exception (e);
                     }
                 }
-            });
+            }));
         }
         
         internal void UnmapDevice (string uuid)
