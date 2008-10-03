6b2ccb202e0755410d0abb5a866d106200042ccf
Gabriel Burt <gabriel.burt@gmail.com>
2008-10-03 17:31:36 +0000

    Add TODO about writing metadata to the DAP file to make sure its
    2008-10-03  Gabriel Burt  <gabriel.burt@gmail.com>
    
    	* src/Dap/Banshee.Dap.MassStorage/Banshee.Dap.MassStorage/MassStorageSource.cs:
    	Add TODO about writing metadata to the DAP file to make sure its
    	MetadataHash will match that of the in-Library track when we load the DAP
    	next time.
    
    
    svn path=/trunk/banshee/; revision=4648

--- src/Dap/Banshee.Dap.MassStorage/Banshee.Dap.MassStorage/MassStorageSource.cs
+++ src/Dap/Banshee.Dap.MassStorage/Banshee.Dap.MassStorage/MassStorageSource.cs
@@ -428,6 +428,11 @@ namespace Banshee.Dap.MassStorage
                 Directory.Create (System.IO.Path.GetDirectoryName (new_uri.LocalPath));
                 File.Copy (fromUri, new_uri, false);
 
+                // TODO If write-to-file is NOT on, then write the metadata in db to the file on the DAP
+                // to ensure that when we load it next time, it's data will match what's in the database
+                // and the MetadataHash will actually match.  This isn't necessary on iPods or MTP since
+                // on them we store our db's metadata in their dbs.
+
                 DatabaseTrackInfo copied_track = new DatabaseTrackInfo (track);
                 copied_track.PrimarySource = this;
                 copied_track.Uri = new_uri;
