fba9e0f054216951e1ae781a31507a5484f4c727
Aaron Bockover <abock@src.gnome.org>
2008-10-21 21:01:44 +0000

    Remove mono-addins for real, collect .mdb files, add share/pkgconfig to search path for finding the SDK mono addins
    svn path=/trunk/banshee/; revision=4710

--- build/osx/collect-deps.sh
+++ build/osx/collect-deps.sh
@@ -13,7 +13,7 @@ mkdir $BUNDLE/gstreamer-0.10
 cp $BUILD_PREFIX/bin/{gst-launch,gst-inspect}-0.10 $BUNDLE &>/dev/null
 find $BUILD_PREFIX/lib -name *.dylib -type f -exec cp {} $BUNDLE \; &>/dev/null
 find $BUILD_PREFIX/lib/gstreamer-0.10 -name *.so -type f -exec cp {} $BUNDLE/gstreamer-0.10 \; &>/dev/null
-find $BUILD_PREFIX/lib/mono -name *.dll -not -name *policy* -type f -exec cp {} $BUNDLE \; &>/dev/null
+find $BUILD_PREFIX/lib/mono -name *.dll* -not -name *policy* -type f -exec cp {} $BUNDLE \; &>/dev/null
 
 popd &>/dev/null
 
