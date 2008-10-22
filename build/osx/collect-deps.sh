28959b95738e4f45aae8ee46636f27a892b3e3cc
Aaron Bockover <abock@src.gnome.org>
2008-10-22 21:55:01 +0000

    Updated, relocation still not fully working
    svn path=/trunk/banshee/; revision=4722

--- build/osx/collect-deps.sh
+++ build/osx/collect-deps.sh
@@ -10,10 +10,32 @@ rm -rf $BUNDLE
 mkdir $BUNDLE
 mkdir $BUNDLE/gstreamer-0.10
 
-cp $BUILD_PREFIX/bin/{gst-launch,gst-inspect}-0.10 $BUNDLE &>/dev/null
-find $BUILD_PREFIX/lib -name *.dylib -type f -exec cp {} $BUNDLE \; &>/dev/null
-find $BUILD_PREFIX/lib/gstreamer-0.10 -name *.so -type f -exec cp {} $BUNDLE/gstreamer-0.10 \; &>/dev/null
-find $BUILD_PREFIX/lib/mono -name *.dll* -not -name *policy* -type f -exec cp {} $BUNDLE \; &>/dev/null
+LIB_PREFIX=$BUILD_PREFIX/lib
 
+#cp $BUILD_PREFIX/bin/{gst-launch,gst-inspect}-0.10 $BUNDLE &>/dev/null
+find $LIB_PREFIX -name *.dylib -type f -exec cp {} $BUNDLE \; &>/dev/null
+#find $LIB_PREFIX/gstreamer-0.10 -name *.so -type f -exec cp {} $BUNDLE/gstreamer-0.10 \; &>/dev/null
+#find $LIB_PREFIX/mono -name *.dll* -not -name *policy* -type f -exec cp {} $BUNDLE \; &>/dev/null
+
+pushd $BUNDLE &>/dev/null
+
+# Rebuild symlinks
+for link in $(find $LIB_PREFIX -name \*.dylib -type l); do
+	ln -s "$(basename $(readlink $link))" "$(basename $link)"
+done
+
+# Relocate libraries
+for dep in $(find . -type f -name \*.dylib -o -name \*.so); do
+	link_deps=$(otool -L $dep | cut -f2 | cut -f1 -d' ')
+	echo "Processing $dep for relocation..."
+	for link_dep in $link_deps; do
+		if [ "x${link_dep:0:${#LIB_PREFIX}}" = "x$LIB_PREFIX" ]; then
+			echo "--> $link_dep"
+			install_name_tool -change $link_dep $(basename $link_dep) $dep
+		fi	
+	done
+done
+
+popd &>/dev/null
 popd &>/dev/null
 
