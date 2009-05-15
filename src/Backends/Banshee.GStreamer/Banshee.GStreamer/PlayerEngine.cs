3a3bda5b0bd955746cf097b99e483ea3a08cb5a5
Gabriel Burt <gabriel.burt@gmail.com>
2009-05-15 11:26:33 -0500

    Fix ReplayGain preference capitalization

--- src/Backends/Banshee.GStreamer/Banshee.GStreamer/PlayerEngine.cs
+++ src/Backends/Banshee.GStreamer/Banshee.GStreamer/PlayerEngine.cs
@@ -501,7 +501,7 @@ namespace Banshee.GStreamer
             }
             
             replaygain_preference = service["general"]["misc"].Add (new SchemaPreference<bool> (ReplayGainEnabledSchema, 
-                Catalog.GetString ("_Enable ReplayGain Correction"),
+                Catalog.GetString ("_Enable ReplayGain correction"),
                 Catalog.GetString ("For tracks that have ReplayGain data, automatically scale (normalize) playback volume."),
                 delegate { ReplayGainEnabled = ReplayGainEnabledSchema.Get (); }
             ));
