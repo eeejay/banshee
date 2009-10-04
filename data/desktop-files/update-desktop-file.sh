e3708919a895f8a980eea646adb366ae49bb7a06
Romain Tartière <romain@blogreen.org>
2009-10-04 17:37:41 +0200

    [build] Remove usage of a GNU sed extension, for FreeBSD portability
    Signed-off-by: Bertrand Lorentz <bertrand.lorentz@gmail.com>

--- data/desktop-files/update-desktop-file.sh
+++ data/desktop-files/update-desktop-file.sh
@@ -21,7 +21,7 @@ if [[ -f "$MIMETYPES_FILE" ]]; then
 
 	if [[ "$UPDATE_MIME_FILE" == "yes" ]]; then
 		(grep -E '^[[:space:]]*#' "$MIMETYPES_FILE";
-			echo "$MIMETYPES" | sed 's,;,\n,g') > "$MIMETYPES_FILE".tmp
+			for t in $(echo "$MIMETYPES" | sed 's,;, ,g'); do echo $t; done) > "$MIMETYPES_FILE".tmp
 		mv "$MIMETYPES_FILE".tmp "$MIMETYPES_FILE"
 	fi
 fi
